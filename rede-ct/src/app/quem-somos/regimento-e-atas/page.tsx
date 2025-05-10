22b6184bd1265ceaf48047e5d84cb4c75ddb9f27
andersonkaiti <anderkaiti@gmail.com>
2025-05-10 20:50:15 -0300

    :sparkles: feat: Página Regimento e Atas finalizada.

--- rede-ct/src/app/quem-somos/regimento-e-atas/page.tsx
+++ rede-ct/src/app/quem-somos/regimento-e-atas/page.tsx
@@ -1,9 +1,56 @@
 export default function RegimentosEAtas() {
   return (
-    <main className="max-w-7x mx-auto flex justify-center p-10">
-      <h1 className="text-5xl font-extrabold">
-        Regimentos, Comunicações, Pautas e Atas
-      </h1>
+    <main className="mx-auto flex max-w-7xl flex-col justify-center gap-12.5 p-10 lg:p-25">
+      <section className="space-y-14">
+        <h1 className="text-center text-5xl font-extrabold">
+          REGIMENTO, CONVOCAÇÕES, PAUTAS E ATAS
+        </h1>
+        <p className="text-justify text-gray-500">
+          Nesta seção do website, a RedeCT mantém três campos distintos: (1) o
+          seu Regimento Interno (atualizado e válido); (2) as convocatórias e
+          pautas das reuniões e assembleias previstas; (3) as Atas das reuniões
+          gerais e setoriais (por exemplo das vice-coordenadorias ou dos GTs).
+        </p>
+      </section>
+      <section className="grid grid-cols-1 gap-14 md:grid-cols-3">
+        <div className="space-y-4">
+          <h2 className="text-4xl font-bold">REGIMENTO INTERNO DA REDECT</h2>
+
+          <p className="text-justify text-gray-500">
+            Esta é a 1ª versão (já válida) do Regimento Interno da RedeCT,
+            publicada em 19/04/2024 (Dia dos Povos Indígenas do Brasil). Durante
+            30 dias, a coordenação da RedeCT receberá sugestões de ajustes pelo
+            e-mail redect.pesquisa@gmail.com após esta data será publicada a
+            versão com as pequenaqs correções sugeridas. Mas, desde 19/04/2024,
+            este Regimento já é válido e deve ser seguido pelos Pesquisadores
+            Filiados da RedeCT.
+          </p>
+        </div>
+        <div className="space-y-4">
+          <h2 className="text-4xl font-bold">
+            CONVOCAÇÕES E PAUTAS DE REUNIÕES AGENDADAS
+          </h2>
+
+          <p className="text-justify text-gray-500">
+            25/07, 15h (on-line) - 1ª Assembleia Geral Extraordinária de
+            Pesquisadores Filiados - PAUTA: (1) Prorrogação do período de
+            submissões de trabalhos no V CCI e seus desdobramentos; (2)
+            Ratificação da aprovação do Regimento Interno da RedeCT, aprovado
+            pelos interessados em 19/04/2024.
+          </p>
+        </div>
+        <div className="space-y-4">
+          <h2 className="text-4xl font-bold">ATAS DE REUNIÕES</h2>
+
+          <p className="text-justify text-gray-500">
+            Nonono nononn nonon ono nn nnononon nonono onno nonon ono on
+            nononono.
+          </p>
+        </div>
+      </section>
+      <section>
+        <h2 className="text-4xl font-bold">ATAS DE REUNIÕES</h2>
+      </section>
     </main>
   );
 }

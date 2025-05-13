7c7499708142194392bd20d3e1090bbc27f506e9
andersonkaiti <anderkaiti@gmail.com>
2025-05-12 23:42:41 -0300

    :recycle: refactor: Tabs de missões, valores e objetivos movido para a tela inicial.

--- rede-ct/src/app/page.tsx
+++ rede-ct/src/app/page.tsx
@@ -1,7 +1,12 @@
+import { MissaoValoresTabs } from "@components/missao-valores-tabs/missao-valores-tabs";
+
 export default function Home() {
   return (
-    <main className="max-w-7x mx-auto flex justify-center p-10">
-      <div>Página inicial</div>
+    <main className="mx-auto flex max-w-7xl flex-col justify-center gap-12.5 p-10 lg:p-25">
+      <h1 className="title-1 text-center">
+        Missão, valores, objetivos e grandes desafios da RedeCT
+      </h1>
+      <MissaoValoresTabs />
     </main>
   );
 }

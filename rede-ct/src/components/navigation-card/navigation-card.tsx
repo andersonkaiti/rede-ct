09f55ad35e2299b6e792e9daea2e4dcfd8fda725
andersonkaiti <anderkaiti@gmail.com>
2025-05-14 09:19:03 -0300

    :sparkles: feat: Adição de botões de navegação branco e azul.

---
+++ rede-ct/src/components/navigation-card/navigation-card.tsx
@@ -0,0 +1,27 @@
+import { ArrowRightIcon } from "lucide-react";
+import Link from "next/link";
+import { INavigationCardProps } from ".";
+import { cn } from "@lib/utils";
+
+export function NavigationCardRoot({
+  children,
+  href,
+  className,
+  ...props
+}: INavigationCardProps) {
+  return (
+    <Link href={href} className="group" {...props}>
+      <div
+        className={cn(
+          "flex items-center gap-4 rounded-md p-6 text-justify shadow-[0_0_10px_rgba(0,0,0,0.1)] transition-all duration-300 hover:shadow-xl",
+          className,
+        )}
+      >
+        {children}
+        <div className="ml-auto">
+          <ArrowRightIcon className="text-indigo-500 transition-all duration-300 group-hover:translate-x-1" />
+        </div>
+      </div>
+    </Link>
+  );
+}

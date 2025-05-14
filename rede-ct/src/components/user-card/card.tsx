66ce12593dc38b8879271af20d8b63a36c93f051
andersonkaiti <anderkaiti@gmail.com>
2025-05-14 09:21:09 -0300

    :recycle: refactor: Renomeação do card de usuário e mudança no botão de currículo Lattes.

--- rede-ct/src/components/user-card/card.tsx
+++ rede-ct/src/components/user-card/card.tsx
@@ -1,6 +1,6 @@
-import { IMembroCardProps } from ".";
+import { IUserCardProps } from ".";
 
-export function MembroCardRoot({ children }: IMembroCardProps) {
+export function UserCardRoot({ children }: IUserCardProps) {
   return (
     <div className="flex flex-1 flex-col items-center justify-center gap-2">
       {children}

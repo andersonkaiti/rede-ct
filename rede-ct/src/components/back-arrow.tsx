e3620bc4b2fe7dc09dce4d622b7e0b1771e97019
andersonkaiti <anderkaiti@gmail.com>
2025-05-14 09:22:53 -0300

    :sparkles: feat: Adição de botão de voltar para a página anterior.

---
+++ rede-ct/src/components/back-arrow.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { ArrowLeftIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+export function BackArrow() {
+  const router = useRouter();
+
+  return (
+    <div
+      className="group -mt-10 flex cursor-pointer items-center gap-2"
+      onClick={() => router.back()}
+    >
+      <ArrowLeftIcon className="h-5 w-5 transition-all duration-300 group-hover:text-indigo-500" />
+      <p className="text-sm font-medium transition-all duration-300 group-hover:text-indigo-500">
+        Voltar
+      </p>
+    </div>
+  );
+}

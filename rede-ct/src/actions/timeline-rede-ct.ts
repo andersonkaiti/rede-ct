08b5d7146ed5559295bd1489cdea68c88ccf5b05
andersonkaiti <anderkaiti@gmail.com>
2025-05-14 09:20:05 -0300

    :sparkles: feat: Página História da RedeCT finalizada.

---
+++ rede-ct/src/actions/timeline-rede-ct.ts
@@ -0,0 +1,96 @@
+const timelineData = [
+  {
+    title: "2002",
+    content:
+      "02/09/2002 – Professores, membros de comunidades tradicionais amazônicas e demais interessados criam a associação Social Desenvolvimento Humano e Comunitário – SocialDHC, com o objetivo de contribuir para a promoção do desenvolvimento humano de comunidades, especialmente de povos tradicionais amazônicos.",
+  },
+  {
+    title: "2003",
+    content:
+      "14/04/2003 – A SocialDHC é qualificada como OSCIP (Organização da Sociedade Civil de Interesse Público) junto ao Ministério da Justiça do Brasil, seguindo-se todos os trâmites da Lei Federal 9.790/99.",
+  },
+  {
+    title: "2014",
+    content:
+      "julho/2014 – Criação do grupo de pesquisa GEDGS (Grupo de Estudos em Democracia e Gestão Social), na UNESP Tupã. O grupo se dedicaria aos estudos sobre povos tradicionais.",
+  },
+  {
+    title: "2017",
+    content:
+      "novembro/2017 – Integrantes do grupo de pesquisa GEDGS/UNESP e da OSCIP SocialDHC planejam a criação da Rede de Pesquisadores RedeCT.",
+  },
+  {
+    title: "2018",
+    content:
+      "22/05/2018 – A proposta de criação da RedeCT foi apresentada ao público dentro do X ENAPEGS, na Universidade Federal do Cariri (UFCA), pelos professores Nelson Russo de Moraes e Alexandre de Castro Campos.",
+  },
+  {
+    title: "2018",
+    content:
+      '15/12/2018 – A RedeCT adquire espaço repositório para livros e coletâneas de seus pesquisadores filiados junto à Editora Fi no formato de série de livros, que é intitulada "Estudos sobre Povos Tradicionais". A série e todas as obras publicadas seguem o padrão e normas do Creative Commons de acesso aberto e gratuito. Disponível em editorafi.org/povos',
+  },
+  {
+    title: "2018",
+    content:
+      '15/12/2018 – A RedeCT publicou (pela Editora da UFRR e pela Editora Fi) o 1º e o 2º volumes de sua coletânea intitulada "Povos Originários e Comunidades Tradicionais: trabalhos de pesquisa e extensão universitária". O volume 1 foi prefaciado pela Profa. Dra. Marta Juliá (Universidad Córdoba/Argentina) e pelo Prof. Dr. Antônio Carlos Sant\'Ana Diegues (USP). O volume 2 foi prefaciado pelo Dr. Geraldo da Silva Gomes (Ministério Público do Tocantins e Fundação Universidade do Tocantins – UNITINS) e pelo Prof. Dr. Nelson Russo de Moraes (UNESP).',
+  },
+  {
+    title: "2019",
+    content:
+      "15/12/2019 – A RedeCT publicou (pela Editora da UFRR e pela Editora Fi) o 3º volume de seu livro/coletânea anual. O prefácio deste volume foi escrito pelo saudoso Prof. Dr. Carlos Rodrigues Brandão (UNICAMP e Universidade Federal de Uberlândia).",
+  },
+  {
+    title: "2020",
+    content:
+      "24 a 27/11/2020 – I Congresso Científico Internacional da RedeCT (I CCI da RedeCT), de modo remoto a partir da FCE/UNESP Tupã.",
+  },
+  {
+    title: "2020",
+    content:
+      "15/12/2020 – Publicação de 3 volumes do livro/coletânea da RedeCT (pela Editora da UFRR e Editora Fi). O prefácio do volume 4 foi escrito pelo Dr. Fernando Guilherme Tenório (Fundação Getúlio Vargas), o prefácio do volume 5 foi do Dr. Flávio Bezerra Barros (UFPA) e o prefácio do 6º volume foi produzido pela Dra. Carine Felkl Prevedello (UFRJ).",
+  },
+  {
+    title: "2021",
+    content:
+      "29/05/2021 – A RedeCT oficializa sua institucionalidade jurídica, passando a ser um projeto da associação OSCIP Social Desenvolvimento Humano e Comunitário – SocialDHC, com nome fantasia: INSTITUTO DE PESQUISAS AMAZÔNICAS E DE POVOS TRADICIONAIS. Passa a ser correta a utilização do termo OSCIP RedeCT.",
+  },
+  {
+    title: "2021",
+    content:
+      "10 a 12/11/2021 – II Congresso Científico Internacional da RedeCT (II CCI da RedeCT) em modo remoto simultaneamente conduzido de Palmas/TO, graças à parceria com a UFT e a UFNT, e por meio dos trabalhos da equipe GEDGS da FCE/UNESP Tupã/SP.",
+  },
+  {
+    title: "2021",
+    content:
+      "15/12/2021 – Foram publicados 4 volumes do livro/coletânea da RedeCT (pela Editora da UFRR e Editora Fi). O volume 7 teve prefácio da Dra. Lorranne Gomes da Silva (UEG), o volume 8 teve prefácio do Dr. Marcelo Rodrigo da Silva (UFRN), o volume 9 teve prefácio do Dr. Patrick Maurice Maury e, por fim, o volume 10 foi prefaciado pelo Dr. Adriano Batista Castorino (UFT).",
+  },
+  {
+    title: "2022",
+    content:
+      "15 a 18/11/2022 – III Congresso Científico Internacional da RedeCT (III CCI da RedeCT), realizado em modo híbrido (com atividades presenciais transmitidas) em Barra do Bugres/MT/Brasil, com a parceria da Faculdade Indígena Intercultural – FAINDI da UNEMAT, e atividades on-line geradas a partir da FCE/UNESP Tupã.",
+  },
+  {
+    title: "2022",
+    content:
+      "15/12/2022 – Ao final deste ano, a RedeCT publicou o volume 11 de seu livro/coletânea (Editora Fi e Editora da UFRR), tendo prefácio do Dr. Alceu Zoia (UNEMAT).",
+  },
+  {
+    title: "2023",
+    content:
+      "07 a 10/11/2023 – IV Congresso Científico Internacional da RedeCT (IV CCI da RedeCT), realizado em modo híbrido (com atividades presenciais transmitidas) em Belém/PA, com a parceria da UNAMA, e atividades on-line geradas a partir da FAAC/UNESP Bauru/SP.",
+  },
+  {
+    title: "2023",
+    content:
+      "15/12/2023 – A RedeCT publicou o volume 12 de sua coleção de capítulos com publicação anual (pela Editora da UFRR e Editora Fi), desta vez com o prefácio da Dra. Raquel Cabral (FAAC/UNESP).",
+  },
+  {
+    title: "2024",
+    content:
+      "19/04/2024 - (Dia dos Povos Indígenas do Brasil) a RedeCT tornou acessível e público o seu website em https://redect.org/novaredect ; publicou e tornou vigente o seu Regimento Interno (disponível no website); definiu que até o final de julho/24 todos os documentos Anais de congresos anteriores estarão publicados; apresentou o novo fluxo de trabalhos científicos por meio dos Eixos Temáticos Permanentes - ETPs e, apresentou a sua nova estrutura, composta por coordenação geral, vice-coordenadorias e suas assessorias, Comitê Legimitimador e Grupos de Trabalho Científicos - GTCs (por sua vez responsaveis pelos Eixos de Trabalho Permanente - ETPs). A apresentação foi ao início do webinário permanente (aberto ao público e transmitido pelo Youtube), às 16h.",
+  },
+];
+
+export async function getTimelineRedeCT() {
+  return timelineData;
+}
---
+++ rede-ct/src/app/quem-somos/apresentacao-e-historia/filiacao/page.tsx
@@ -0,0 +1,127 @@
+import { BackArrow } from "@components/back-arrow";
+import { OrderedList } from "@components/ordered-list";
+import { FileText, Users, Building2, Mail, ArrowLeft } from "lucide-react";
+import Link from "next/link";
+
+export default function Filiacao() {
+  return (
+    <main className="mx-auto flex max-w-7xl flex-col justify-center gap-12.5 p-5 py-8 lg:p-25">
+      <BackArrow />
+      <section className="space-y-14">
+        <div className="flex items-center justify-center gap-4">
+          <FileText className="h-8 w-8 text-indigo-500" />
+          <h1 className="title-1 text-center">Como se filiar à RedeCT?</h1>
+        </div>
+      </section>
+
+      <section className="space-y-14">
+        <div className="flex items-center gap-4">
+          <Building2 className="h-6 w-6 text-indigo-500" />
+          <h2 className="title-2">A RedeCT atual</h2>
+        </div>
+        <div className="space-y-7">
+          <p className="text-justify">
+            A RedeCT é uma Rede de Pesquisadores mantida pela OSCIP SocialDHC
+            (Instituto de Pesquisas Amazônicas e de Povos Tradicionais) e que
+            possui sede administrativa e jurídica no município de Porto Nacional
+            – Estado do Tocantins – Brasil.
+          </p>
+          <div className="grid gap-4 md:grid-cols-2">
+            <div className="flex items-center gap-4">
+              <Users className="h-5 w-5 text-indigo-500" />
+              <p>Presidente voluntária da OSCIP: Joicileia Juliate Fonseca</p>
+            </div>
+            <div className="flex items-center gap-4">
+              <Users className="h-5 w-5 text-indigo-500" />
+              <p>
+                Coordenador voluntário da RedeCT: Prof. Dr. Nelson Russo de
+                Moraes
+              </p>
+            </div>
+            <div className="flex items-center gap-4">
+              <Mail className="h-5 w-5 text-indigo-500" />
+              <p>
+                E-mails de contato: redect.pesquisa@gmail.com e
+                contato@redect.org
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="space-y-14">
+        <div className="flex items-center gap-4">
+          <FileText className="h-6 w-6 text-indigo-500" />
+          <h2 className="title-2">Processo de Filiação</h2>
+        </div>
+        <div className="space-y-7">
+          <OrderedList.Root>
+            <OrderedList.Item>
+              Conheça a RedeCT, sua missão, seus objetivos e o seu Regimento
+              Interno;
+            </OrderedList.Item>
+            <OrderedList.Item>
+              Preencha o formulário de solicitação de filiação pelo link
+              https://forms.gle/7pQWnnnHpC4zKeij7
+            </OrderedList.Item>
+            <OrderedList.Item>
+              Realize o pagamento de sua 1a anuidade (referente ao ano de 2024 -
+              de acordo com sua classificação de Pesquisador Filiado). Pela
+              chave pix da231105-2947-4343-9214-064b15e45313 (Alexandre de
+              Castro Campos).
+            </OrderedList.Item>
+            <OrderedList.Item>
+              Atenção: se você fizer 1º o pagamento de sua inscrição para o V
+              CCI da RedeCT (acesso pelo link
+              https://www.even3.com.br/v-congresso-cientifico-internacional-da-redect-447704/
+              ), você pode realizar o pagamento da anuidade com a dedução de
+              R$50,00 (se fizer o inverso, a RedeCT não realizará devolução do
+              valor da inscrição).
+            </OrderedList.Item>
+            <OrderedList.Item>
+              Envie os comprovantes de pagamentos para o e-mail
+              filiados@redect.org
+            </OrderedList.Item>
+            <OrderedList.Item>
+              Caso você realize o pagamento da anuidade de uma conta-corrente
+              que não é sua, informe isso no e-mail para a correta concialiação
+              bancária na RedeCT.
+            </OrderedList.Item>
+          </OrderedList.Root>
+        </div>
+      </section>
+
+      <section className="space-y-14">
+        <div className="flex items-center gap-4">
+          <Users className="h-6 w-6 text-indigo-500" />
+          <h2 className="title-2">Classificação dos Pesquisadores</h2>
+        </div>
+        <div className="space-y-7">
+          <p className="text-justify">
+            Os Pesquisadores Filiados são classificados como:
+          </p>
+          <OrderedList.Root>
+            <OrderedList.Item>
+              Pesquisador Senior (Doutor ou Livre-docente)
+            </OrderedList.Item>
+            <OrderedList.Item>
+              Pesquisador (Mestre ou profissional com graduação concluída)
+            </OrderedList.Item>
+            <OrderedList.Item>
+              Pesquisador Júnior (estudante de graduação ou mesmo de ensino
+              médio, que tenham mais de 18 anos de idade)
+            </OrderedList.Item>
+          </OrderedList.Root>
+        </div>
+      </section>
+
+      <Link
+        href="/quem-somos/apresentacao-e-historia"
+        className="flex items-center gap-2 text-indigo-500 hover:text-indigo-600"
+      >
+        <ArrowLeft className="h-5 w-5" />
+        Voltar para a página de apresentação e história
+      </Link>
+    </main>
+  );
+}

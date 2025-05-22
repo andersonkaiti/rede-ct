f14ca2688406e6800993fe1a9bd1cdffb252f06e
andersonkaiti <anderkaiti@gmail.com>
2025-05-22 09:00:20 -0300

    :recycle: refactor: Uso apenas do skeleton do Shadcn.

--- src/app/(auth)/_components/user-profile/user-profile-skeleton.tsx
+++ src/app/(auth)/_components/user-profile/user-profile-skeleton.tsx
@@ -1,16 +1,16 @@
-import { Skeleton } from "@components/skeleton";
+import { Skeleton } from "@components/ui/skeleton";
 import { User } from "lucide-react";
 
 export function UserProfileSkeleton() {
   return (
-    <Skeleton.Root className="flex items-center justify-between gap-2 px-2">
+    <Skeleton className="flex items-center justify-between gap-2 px-2">
       <div className="flex items-center gap-2">
-        <div className="flex h-9 w-9 items-center justify-center rounded-sm bg-gray-300 dark:bg-gray-700">
-          <User className="h-5 w-5 text-gray-200 dark:text-gray-600" />
+        <div className="flex h-9 w-9 items-center justify-center rounded-sm bg-gray-300">
+          <User className="h-5 w-5 text-gray-200" />
         </div>
-        <Skeleton.Text className="h-4 w-24" />
+        <div className="h-4 w-24 rounded-full bg-gray-200" />
       </div>
-      <Skeleton.Circle className="h-4 w-2" />
-    </Skeleton.Root>
+      <div className="h-4 w-2 rounded-full bg-gray-200" />
+    </Skeleton>
   );
 }

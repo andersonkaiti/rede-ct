40ab75982ec8ee703295fe4bab3d28661e06fede
andersonkaiti <anderkaiti@gmail.com>
2025-06-16 23:05:12 -0300

    :lipstick: style: Separação do rodapé em componentes e melhoria na estilização.

---
+++ src/app/(public)/_components/footer/_helpers/links.ts
@@ -0,0 +1,145 @@
+import * as Icons from "lucide-react";
+import {
+  NavigationLink,
+  NavigationLink as NavigationLinkType,
+} from "types/navigation-link";
+
+export const navigationLinks: NavigationLinkType[] = [
+  {
+    path: "/",
+    label: "Home",
+    icon: Icons.Home,
+  },
+  {
+    label: "Quem Somos",
+    children: [
+      {
+        path: "/quem-somos/apresentacao-e-historia",
+        label: "Apresentação e História",
+        icon: Icons.Info,
+      },
+      {
+        path: "/quem-somos/equipe-de-gestao",
+        label: "Equipe de Gestão",
+        icon: Icons.Users,
+      },
+      {
+        path: "/quem-somos/pesquisadores-participantes",
+        label: "Pesquisadores Participantes",
+        icon: Icons.GraduationCap,
+      },
+      {
+        path: "/quem-somos/eixos-tematicos-permanentes",
+        label: "Eixos Temáticos Permanentes",
+        icon: Icons.BookOpen,
+      },
+      {
+        path: "/quem-somos/regimento-e-atas",
+        label: "Regimento e Atas",
+        icon: Icons.FileText,
+      },
+      {
+        path: "/quem-somos/parceiros-e-financiadores",
+        label: "Parceiros e Financiadores",
+        icon: Icons.UsersRound,
+      },
+    ],
+  },
+  {
+    label: "Divisão Científica",
+    children: [
+      {
+        path: "/divisao-cientifica/congresso-cientifico-internacional",
+        label: "Congresso Científico Internacional",
+        icon: Icons.Globe,
+      },
+      {
+        path: "/divisao-cientifica/congressos-regionais",
+        label: "Congressos Regionais",
+        icon: Icons.Map,
+      },
+      {
+        path: "/divisao-cientifica/webinario-permanente",
+        label: "Webinário Permanente",
+        icon: Icons.Network,
+      },
+      {
+        path: "/divisao-cientifica/cursos-e-capacitacoes",
+        label: "Cursos e Capacitações",
+        icon: Icons.GraduationCap,
+      },
+      {
+        path: "/divisao-cientifica/disciplinas-e-cursos-de-pos-graduacao",
+        label: "Disciplinas e cursos de pós-graduação",
+        icon: Icons.BookOpen,
+      },
+      {
+        path: "/divisao-cientifica/selo-de-qualidade-em-extensao-universitaria",
+        label: "Selo de Qualidade em Extensão Universitária",
+        icon: Icons.Star,
+      },
+      {
+        path: "/divisao-cientifica/calendario-de-eventos",
+        label: "Calendário de Eventos",
+        icon: Icons.ScrollText,
+      },
+    ],
+  },
+  {
+    label: "Publicações",
+    children: [
+      {
+        path: "/publicacoes/periodico-e-revistas-parceiras",
+        label: "Periódico da RedeCT e Revistas Parceiras",
+        icon: Icons.FileText,
+      },
+      {
+        path: "/publicacoes/livros-da-redect",
+        label: "Coletânea da RedeCT (Livro da RedeCT)",
+        icon: Icons.BookOpen,
+      },
+      {
+        path: "/publicacoes/livros-e-capitulos",
+        label: "Livros e Capítulos de Interesse",
+        icon: Icons.BookOpen,
+      },
+      {
+        path: "/publicacoes/artigos-cientificos",
+        label: "Artigos Científicos de Interesse",
+        icon: Icons.FileText,
+      },
+    ],
+  },
+  {
+    path: "/noticias",
+    label: "Notícias",
+    icon: Icons.Newspaper,
+  },
+];
+
+export function getAllLinks() {
+  const links: NavigationLink[] = [];
+
+  navigationLinks.forEach((link) => {
+    if (link.path) {
+      links.push(link);
+    }
+
+    if (link.children) {
+      link.children.forEach((child) => {
+        if (child.path) {
+          links.push(child);
+        }
+      });
+    }
+  });
+
+  const middle = Math.ceil(links.length / 2);
+  const firstColumn = links.slice(0, middle);
+  const secondColumn = links.slice(middle);
+
+  return {
+    firstColumn,
+    secondColumn,
+  };
+}
--- src/app/(public)/_components/footer/footer.tsx
+++ src/app/(public)/_components/footer/footer.tsx
@@ -1,14 +1,14 @@
-import { Badge } from "@components/ui/badge";
-import { MailIcon } from "lucide-react";
 import Image from "next/image";
-import Link from "next/link";
+
+import Links from "./_components/links";
+import { SocialMedia } from "./_components/social-media";
 
 export function Footer() {
   return (
-    <footer className="bg-[#171717]">
+    <footer className="bg-[#171717] text-white">
       <div className="mx-auto w-full max-w-screen-xl space-y-8 p-4 py-6 sm:space-y-20 lg:py-8">
         <div className="gap-16 space-y-10 md:flex md:justify-between">
-          <div className="space-y-8">
+          <div className="max-w-xs space-y-8">
             <div className="flex items-center gap-2">
               <div className="relative h-10 w-10">
                 <Image
@@ -18,189 +18,26 @@ export function Footer() {
                   alt="Rede CT"
                 />
               </div>
-              <h3 className="text-2xl text-white">RedeCT</h3>
+              <h3 className="text-2xl font-semibold text-white">RedeCT</h3>
             </div>
-            <h1 className="text-sm text-[#9CA3AF]">
+            <h1 className="text-sm text-white">
               RedeCT - Rede Internacional de Pesquisadores sobre Povos
               Originários e Comunidades Tradicionais
             </h1>
-            <div className="space-y-2 text-white">
-              <div className="flex gap-2">
-                <Link
-                  href="https://www.facebook.com/redect01/"
-                  target="_blank"
-                  className="hover:bg-primary flex items-center justify-center rounded-md p-1 transition-all duration-300"
-                >
-                  <div className="relative size-5">
-                    <Image
-                      src="/images/icons/facebook-brands.svg"
-                      alt="Facebook"
-                      fill
-                      className="object-cover"
-                    />
-                  </div>
-                </Link>
-                <Link
-                  href="/"
-                  target="_blank"
-                  className="hover:bg-primary flex items-center justify-center rounded-md p-1 transition-all duration-300"
-                >
-                  <div className="relative size-5">
-                    <Image
-                      src="/images/icons/youtube-brands.svg"
-                      alt="Youtube"
-                      fill
-                      className="object-cover"
-                    />
-                  </div>
-                </Link>
-                <Link
-                  href="https://www.instagram.com/redecomunidadestradicionais/"
-                  target="_blank"
-                  className="hover:bg-primary rounded-md p-1 transition-all duration-300"
-                >
-                  <div className="relative size-5">
-                    <Image
-                      src="/images/icons/instagram-brands.svg"
-                      alt="Instagram"
-                      fill
-                      className="object-cover"
-                    />
-                  </div>
-                </Link>
-              </div>
-              <div className="flex items-center gap-2 text-sm">
-                <Badge className="bg-primary rounded-full p-1">
-                  <MailIcon className="!size-4 text-white" />
-                </Badge>
-                contato@redect.org
-              </div>
-            </div>
           </div>
           <div>
-            <h2 className="text-primary mb-6 text-sm font-semibold uppercase">
-              MAPA
+            <h2 className="mb-6 text-lg font-semibold uppercase">Navegação</h2>
+            <Links />
+          </div>
+          <div>
+            <h2 className="mb-6 text-lg font-semibold uppercase">
+              Redes Sociais
             </h2>
-            <div className="grid gap-4 sm:grid-cols-2 sm:gap-8">
-              <ul className="space-y-4 text-gray-400">
-                <li>
-                  <Link
-                    href="/"
-                    target="_blank"
-                    className="group hover:text-primary flex items-center text-sm transition-all duration-300"
-                  >
-                    <div className="bg-primary mr-3 h-1 w-1 rounded-full transition-all duration-200 group-hover:w-2" />
-                    HOME
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/"
-                    target="_blank"
-                    className="group hover:text-primary flex items-center text-sm transition-all duration-300"
-                  >
-                    <div className="bg-primary mr-3 h-1 w-1 rounded-full transition-all duration-200 group-hover:w-2" />
-                    Apresentação, história e logomarca da RedeCT
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/"
-                    target="_blank"
-                    className="group hover:text-primary flex items-center text-sm transition-all duration-300"
-                  >
-                    <div className="bg-primary mr-3 h-1 w-1 rounded-full transition-all duration-200 group-hover:w-2" />
-                    Missão, valores e objetivos da RedeCT
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/"
-                    target="_blank"
-                    className="group hover:text-primary flex items-center text-sm transition-all duration-300"
-                  >
-                    <div className="bg-primary mr-3 h-1 w-1 rounded-full transition-all duration-200 group-hover:w-2" />
-                    Equipe voluntária de gestão
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/"
-                    target="_blank"
-                    className="group hover:text-primary flex items-center text-sm transition-all duration-300"
-                  >
-                    <div className="bg-primary mr-3 h-1 w-1 rounded-full transition-all duration-200 group-hover:w-2" />
-                    Pesquisadores participantes
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/"
-                    target="_blank"
-                    className="group hover:text-primary flex items-center text-sm transition-all duration-300"
-                  >
-                    <div className="bg-primary mr-3 h-1 w-1 rounded-full transition-all duration-200 group-hover:w-2" />
-                    Eixos Temáticos Permanente (ETPs)
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/"
-                    target="_blank"
-                    className="group hover:text-primary flex items-center text-sm transition-all duration-300"
-                  >
-                    <div className="bg-primary mr-3 h-1 w-1 rounded-full transition-all duration-200 group-hover:w-2" />
-                    Regimento e atas
-                  </Link>
-                </li>
-              </ul>
-              <ul className="space-y-4 text-gray-400">
-                <li>
-                  <Link
-                    href="/"
-                    target="_blank"
-                    className="group hover:text-primary flex items-center text-sm transition-all duration-300"
-                  >
-                    <div className="bg-primary mr-3 h-1 w-1 rounded-full transition-all duration-200 group-hover:w-2" />
-                    Parceiros e financiadores
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/"
-                    target="_blank"
-                    className="group hover:text-primary flex items-center text-sm transition-all duration-300"
-                  >
-                    <div className="bg-primary mr-3 h-1 w-1 rounded-full transition-all duration-200 group-hover:w-2" />
-                    Controle Social e Transparência
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/"
-                    target="_blank"
-                    className="group hover:text-primary flex items-center text-sm transition-all duration-300"
-                  >
-                    <div className="bg-primary mr-3 h-1 w-1 rounded-full transition-all duration-200 group-hover:w-2" />
-                    Instituto de Pesquisas Amazônicas e de Povos Tradicionais
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/"
-                    target="_blank"
-                    className="group hover:text-primary flex items-center text-sm transition-all duration-300"
-                  >
-                    <div className="bg-primary mr-3 h-1 w-1 rounded-full transition-all duration-200 group-hover:w-2" />
-                    NOTÍCIAS
-                  </Link>
-                </li>
-              </ul>
-            </div>
+            <SocialMedia />
           </div>
         </div>
         <div className="justify-center sm:flex sm:items-center">
-          <span className="text-sm text-gray-400 sm:text-center">
+          <span className="text-sm text-white sm:text-center">
             © 2025 REDE CT
           </span>
         </div>

f651dd7c0468949892311e52126d8d5af556b2d7
andersonkaiti <anderkaiti@gmail.com>
2025-08-24 22:32:40 -0300

    :recycle: refactor(news): Reestruturação dos formulários de cadastro e edição de notícias, removendo componentes desnecessários e melhorando a validação de erros.

--- src/app/(protected)/area-restrita/noticias/cadastrar/page.tsx
+++ src/app/(protected)/area-restrita/noticias/cadastrar/page.tsx
@@ -2,7 +2,6 @@
 
 import { Alert, AlertDescription } from '@components/ui/alert'
 import { Button } from '@components/ui/button'
-import { Card, CardContent, CardHeader, CardTitle } from '@components/ui/card'
 import { FileUpload } from '@components/ui/file-upload'
 import { Input } from '@components/ui/input'
 import { Label } from '@components/ui/label'
@@ -12,10 +11,11 @@ import {
   PageForm,
   PageFormContent,
   PageFormContentField,
+  PageHeaderContent,
   PageTitle,
 } from '@components/ui/page-container'
 import { Textarea } from '@components/ui/textarea'
-import { AlertCircle, Loader2, Newspaper } from 'lucide-react'
+import { AlertCircle, Loader2 } from 'lucide-react'
 import { useRegisterNews } from '../_hooks/use-register-news.hook'
 
 export default function CadastrarNoticia() {
@@ -23,86 +23,74 @@ export default function CadastrarNoticia() {
 
   return (
     <PageContainer>
-      <header className="space-y-4">
-        <PageTitle>Informações da Notícia</PageTitle>
+      <PageHeaderContent>
+        <PageTitle>Cadastrar Notícia</PageTitle>
         <PageDescription>
-          Preencha os campos abaixo para cadastrar uma nova notícia
+          Preencha os campos abaixo para cadastrar uma nova notícia.
         </PageDescription>
-      </header>
+      </PageHeaderContent>
 
-      <Card className="rounded-md shadow-xs">
-        <CardHeader className="flex items-center gap-2">
-          <Newspaper className="!size-5 text-primary" />
-          <CardTitle className="text-xl">Cadastrar Notícia</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <PageForm action={formAction}>
-            <PageFormContent>
-              <PageFormContentField>
-                <Label>
-                  Título <span className="text-red-500">*</span>
-                </Label>
+      <PageForm action={formAction}>
+        <PageFormContent>
+          <PageFormContentField>
+            <Label>
+              Título <span className="text-primary">*</span>
+            </Label>
 
-                <Input
-                  defaultValue={payload?.get('title') as string}
-                  name="title"
-                  placeholder="Digite o título da notícia"
-                />
+            <Input
+              defaultValue={payload?.get('title') as string}
+              name="title"
+              placeholder="Digite o título da notícia"
+            />
 
-                {errors?.title && errors?.title && (
-                  <Alert className="border-red-500 p-2" variant="destructive">
-                    <AlertCircle className="size-4" />
-                    <AlertDescription>{errors?.title}</AlertDescription>
-                  </Alert>
-                )}
-              </PageFormContentField>
+            {errors?.title && errors?.title && (
+              <Alert className="border-primary p-2" variant="destructive">
+                <AlertCircle className="size-4" />
+                <AlertDescription>{errors?.title}</AlertDescription>
+              </Alert>
+            )}
+          </PageFormContentField>
 
-              <PageFormContentField>
-                <Label>
-                  Texto <span className="text-red-500">*</span>
-                </Label>
+          <PageFormContentField>
+            <Label>
+              Imagem <span className="text-primary">*</span>
+            </Label>
 
-                <Textarea
-                  defaultValue={payload?.get('content') as string}
-                  name="content"
-                  placeholder="Digite o conteúdo da notícia"
-                />
+            <FileUpload maxSizeMB={5} />
 
-                {errors?.content && errors?.content && (
-                  <Alert className="border-red-500 p-2" variant="destructive">
-                    <AlertCircle className="size-4" />
-                    <AlertDescription>{errors?.content}</AlertDescription>
-                  </Alert>
-                )}
-              </PageFormContentField>
+            {errors?.image && errors?.image && (
+              <Alert className="border-primary p-2" variant="destructive">
+                <AlertCircle className="size-4" />
+                <AlertDescription>{errors?.image}</AlertDescription>
+              </Alert>
+            )}
+          </PageFormContentField>
 
-              <PageFormContentField>
-                <Label>
-                  Imagem <span className="text-red-500">*</span>
-                </Label>
+          <PageFormContentField>
+            <Label>
+              Texto <span className="text-primary">*</span>
+            </Label>
 
-                <FileUpload maxSizeMB={5} />
+            <Textarea
+              defaultValue={payload?.get('content') as string}
+              name="content"
+              placeholder="Digite o conteúdo da notícia"
+            />
 
-                {errors?.image && errors?.image && (
-                  <Alert className="border-red-500 p-2" variant="destructive">
-                    <AlertCircle className="size-4" />
-                    <AlertDescription>{errors?.image}</AlertDescription>
-                  </Alert>
-                )}
-              </PageFormContentField>
+            {errors?.content && errors?.content && (
+              <Alert className="border-primary p-2" variant="destructive">
+                <AlertCircle className="size-4" />
+                <AlertDescription>{errors?.content}</AlertDescription>
+              </Alert>
+            )}
+          </PageFormContentField>
 
-              <Button
-                className="cursor-pointer"
-                disabled={isLoading}
-                type="submit"
-              >
-                {isLoading && <Loader2 className="size-4 animate-spin" />}
-                Cadastrar notícia
-              </Button>
-            </PageFormContent>
-          </PageForm>
-        </CardContent>
-      </Card>
+          <Button className="cursor-pointer" disabled={isLoading} type="submit">
+            {isLoading && <Loader2 className="size-4 animate-spin" />}
+            Cadastrar notícia
+          </Button>
+        </PageFormContent>
+      </PageForm>
     </PageContainer>
   )
 }

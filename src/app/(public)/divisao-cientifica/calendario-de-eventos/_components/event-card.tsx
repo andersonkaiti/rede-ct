4991161ed695041b2e7a170c084680c581b496ae
andersonkaiti <anderkaiti@gmail.com>
2025-08-25 20:08:29 -0300

    :sparkles: feat(events): :sparkles: feat(courses): Implementação de novos componentes de carregamento, com melhorias na estrutura e responsividade do EventCard, além da adição de novos dados de eventos no mock.

--- src/app/(public)/divisao-cientifica/calendario-de-eventos/_components/event-card.tsx
+++ src/app/(public)/divisao-cientifica/calendario-de-eventos/_components/event-card.tsx
@@ -1,83 +1,64 @@
-import { Badge } from '@components/ui/badge'
 import { Button } from '@components/ui/button'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@components/ui/card'
+import { CardTitle } from '@components/ui/card'
 import type { IEvent } from '@mocks/events/events'
-import { ArrowRight, Calendar, Clock } from 'lucide-react'
+import { ArrowRight, Clock } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 
 export function EventCard({
-  event: {
-    title,
-    description,
-    href,
-    image,
-    status,
-    subscriptionPeriod,
-    subtitle,
-  },
+  event: { title, href, image, status, subscriptionPeriod, subtitle },
 }: {
   event: IEvent
 }) {
   return (
-    <Card className="flex flex-col items-stretch gap-2 rounded-lg p-0 shadow-sm md:flex-row">
-      <picture className="relative h-64 w-full overflow-hidden rounded-t-lg md:h-auto md:w-2/4 md:rounded-l-lg md:rounded-tr-none">
-        <Image
-          alt={image.alt}
-          className="absolute h-full w-full object-cover"
-          fill
-          src={image.url}
-        />
-        <div className="absolute inset-0 bg-gradient-to-t from-black/20 to-transparent" />
-      </picture>
+    <div className="flex flex-col gap-2">
+      <header className="h-80">
+        <picture className="relative flex size-full overflow-hidden rounded-md border-1 border-muted-foreground">
+          <Image
+            alt={image.alt}
+            className="object-cover"
+            fill
+            src={image.url}
+          />
+          <div className="absolute inset-0 bg-gradient-to-t from-black/40 to-transparent dark:from-black/70 dark:to-transparent" />
+        </picture>
+      </header>
 
-      <div className="w-full space-y-4 p-6 md:w-3/4">
-        <CardHeader className="space-y-4 p-0">
-          <time className="flex items-stretch gap-2 text-sm">
-            <Badge className="rounded-md bg-primary/20 p-1 font-bold text-primary">
-              <Calendar className="!size-5 h-fit text-primary" />
-            </Badge>
-            <div className="flex flex-col leading-4">
-              <span className="font-bold text-foreground">
-                Período de inscrição: {subscriptionPeriod.start} até{' '}
-                {subscriptionPeriod.end}
-              </span>
-              <div className="flex items-center gap-2">
-                <Clock className="!size-3 text-muted-foreground" />
-                <span className="text-muted-foreground">
-                  {subscriptionPeriod.time}
-                </span>
-              </div>
-            </div>
+      <div className="flex h-fit flex-grow flex-col justify-between gap-4 py-2">
+        <div className="space-y-4">
+          <time className="flex items-center gap-2 text-sm leading-4">
+            <span className="text-muted-foreground">
+              Período de inscrição: {subscriptionPeriod.start} até{' '}
+              {subscriptionPeriod.end}
+            </span>
           </time>
 
-          <CardTitle className="font-bold text-xl">
+          <CardTitle className="font-semibold text-2xl">
             {title} ({subtitle})
           </CardTitle>
-        </CardHeader>
-        <CardContent className="p-0">
-          <CardDescription className="text-justify text-muted-foreground">
-            {description}
-          </CardDescription>
-        </CardContent>
-        <CardFooter className="p-0">
-          <Link className="w-full" href={href}>
-            <Button className="group w-full font-bold">
+        </div>
+
+        <div className="space-y-2">
+          <h4 className="w-fit font-semibold text-sm">Informações</h4>
+          <div className="flex items-center">
+            <Clock className="!size-4 mr-1.5 text-muted-foreground" />
+            <div className="font-semibold text-muted-foreground text-sm leading-0.5">
+              {subscriptionPeriod.time}
+            </div>
+          </div>
+        </div>
+
+        <footer>
+          <Button asChild className="group w-full font-bold" variant="outline">
+            <Link className="w-full" href={href}>
               {status === 'inscricoes-abertas'
                 ? 'Inscreva-se'
                 : 'Inscrições encerradas'}
               <ArrowRight className="!size-4 transition-all duration-200 group-hover:translate-x-1" />
-            </Button>
-          </Link>
-        </CardFooter>
+            </Link>
+          </Button>
+        </footer>
       </div>
-    </Card>
+    </div>
   )
 }
--- src/app/(public)/divisao-cientifica/calendario-de-eventos/page.tsx
+++ src/app/(public)/divisao-cientifica/calendario-de-eventos/page.tsx
@@ -12,7 +12,7 @@ export default function CalendarioDeEventos() {
     <main className="mx-auto flex max-w-7xl flex-col justify-center gap-12.5 p-5 py-10 lg:p-25">
       <header className="space-y-8">
         <h1 className="title-2">Calendário de eventos</h1>
-        <p className="text-justify text-lg text-muted-foreground md:text-center">
+        <p className="text-justify text-lg text-muted-foreground">
           Nesta seção são divulgados os eventos científicos relacionados direta
           ou indiretamente à pauta central da RedeCT (também os eventos que não
           são conduzidos pelos Pesquisadores Filiados, mas de interesse destes).

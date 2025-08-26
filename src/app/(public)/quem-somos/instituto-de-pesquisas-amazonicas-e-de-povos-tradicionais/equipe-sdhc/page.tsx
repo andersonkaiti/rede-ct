706dc446fae13738e07251c963d206d66570355d
andersonkaiti <anderkaiti@gmail.com>
2025-08-26 10:18:06 -0300

    :lipstick: style(equipe-sdhc): Refatoração dos componentes da equipe SDHC para utilizar UserCard e UserCardSkeleton, além de melhorias na estrutura e estilo do layout.

--- src/app/(public)/quem-somos/instituto-de-pesquisas-amazonicas-e-de-povos-tradicionais/equipe-sdhc/page.tsx
+++ src/app/(public)/quem-somos/instituto-de-pesquisas-amazonicas-e-de-povos-tradicionais/equipe-sdhc/page.tsx
@@ -1,7 +1,7 @@
 import { BackArrow } from '@components/back-arrow'
+import { UserCardRedLine } from '@components/ui/user-card'
 import dynamic from 'next/dynamic'
 import { Suspense } from 'react'
-
 import { LoadingSkeleton } from './_components/loading-skeleton'
 
 const DynamicEquipeSdhc = dynamic(() =>
@@ -12,10 +12,15 @@ export default function EquipeSdhc() {
   return (
     <main className="mx-auto flex max-w-7xl flex-col justify-center gap-12.5 p-5 py-8 lg:p-25">
       <BackArrow />
-      <h1 className="title-2">
-        Atual equipe de gestão da associação Social Desenvolvimento Humano e
-        Comunitário:
-      </h1>
+
+      <div className="space-y-8">
+        <h1 className="title-2 text-center">
+          Atual equipe de gestão da associação Social Desenvolvimento Humano e
+          Comunitário:
+        </h1>
+        <UserCardRedLine />
+      </div>
+
       <Suspense fallback={<LoadingSkeleton />}>
         <DynamicEquipeSdhc />
       </Suspense>

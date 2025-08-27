44e1b80c58b1acbd9e9d93efa722fcd56501f7fc
andersonkaiti <anderkaiti@gmail.com>
2025-08-27 08:42:49 -0300

    :lipstick: style(sidebar,user-profile): Melhoria na apresentação do componente UserProfile com a adição de iniciais do usuário e formatação do nome completo, além de ajustes nas cores do Sidebar.

--- src/app/(protected)/_components/sidebar/user-profile/user-profile.tsx
+++ src/app/(protected)/_components/sidebar/user-profile/user-profile.tsx
@@ -8,10 +8,15 @@ import {
   MenubarMenu,
   MenubarTrigger,
 } from '@components/ui/menubar'
+import { Separator } from '@components/ui/separator'
 import { ChevronsUpDown, LogOut } from 'lucide-react'
 
 export async function UserProfile() {
   const user = await currentUser()
+
+  const firstInitial = user?.firstName?.charAt(0) ?? ''
+  const lastInitial = user?.lastName?.charAt(0) ?? ''
+  const fullName = [user?.firstName, user?.lastName].filter(Boolean).join(' ')
 
   return (
     <Menubar className="flex w-full border-none shadow-none">
@@ -21,20 +26,31 @@ export async function UserProfile() {
             <Avatar>
               <AvatarImage src={user?.imageUrl || ''} />
               <AvatarFallback>
-                {user?.firstName?.charAt(0)}
-                {user?.lastName?.charAt(0)}
+                {firstInitial}
+                {lastInitial}
               </AvatarFallback>
             </Avatar>
-            {user?.firstName} {user?.lastName}
+            {fullName}
           </div>
           <ChevronsUpDown className="ml-2 h-4 w-4" />
         </MenubarTrigger>
-        <MenubarContent align="end" className="bg-background">
-          <MenubarItem>
+        <MenubarContent align="end" className="w-fit space-y-2 bg-background">
+          <div className="px-2">
+            <div className="font-sm text-foreground">E-mail</div>
+            {user?.emailAddresses?.[0]?.emailAddress && (
+              <div className="break-all text-muted-foreground text-xs">
+                {user.emailAddresses[0].emailAddress}
+              </div>
+            )}
+          </div>
+
+          <Separator />
+
+          <MenubarItem className="cursor-pointer">
             <SignOutButton>
               <div className="group flex w-full cursor-pointer items-center gap-2 text-primary">
-                <LogOut className="size-5 text-primary" />
-                Deslogar
+                <LogOut className="size-3 text-primary" />
+                <span className="text-xs">Deslogar</span>
               </div>
             </SignOutButton>
           </MenubarItem>

1e341bda7ef8f1697e5f3622bb6271c210478770
andersonkaiti <anderkaiti@gmail.com>
2025-08-28 09:06:09 -0300

    ✨ feat(etps): Refatoração da página e dos componentes de Eixos Temáticos Permanentes para melhorar a apresentação das informações, incluindo a adição dos detalhes da coordenação e dos membros do grupo na estrutura de accordion.

--- src/@types/etp.d.ts
+++ src/@types/etp.d.ts
@@ -1,14 +1,29 @@
+interface IETPUser {
+  first_name: string
+  last_name: string
+  profile_image_url?: string
+  lattesUrl?: string
+  orcid?: string
+  email?: string
+  institution?: string
+  title?: string
+  notes?: string
+}
+
 export interface IETP {
   name: string
   description: string
+  text: string
   members: {
     role: string
-    name: string
-    image: {
-      src: string
-      alt: string
-    }
-    lattesUrl: string
+    user: IETPUser
   }[]
-  text?: string
+  gtMembers?: {
+    name: string
+    role?: string
+    registration?: string
+    institution?: string
+    notes?: string
+  }[]
+  gtText: string
 }

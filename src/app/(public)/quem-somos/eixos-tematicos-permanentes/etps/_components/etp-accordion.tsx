5906ac2109216be3083afb088b21bc8fa044a73e
andersonkaiti <anderkaiti@gmail.com>
2025-08-30 09:40:09 -0300

    ✨ feat(etp-accordion): Adição de filtro e loading skeleton, além de refatoração do componente EPTAccordion para suportar a nova funcionalidade de filtragem de ETPs.

--- src/app/(public)/quem-somos/eixos-tematicos-permanentes/etps/_components/etp-accordion.tsx
+++ src/app/(public)/quem-somos/eixos-tematicos-permanentes/etps/_components/etp-accordion.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import {
   Accordion,
   AccordionContent,
@@ -5,38 +7,82 @@ import {
   AccordionTrigger,
 } from '@components/ui/accordion'
 import { getEtps } from '@mocks/etps'
+import { useQuery } from '@tanstack/react-query'
+import { useQueryState } from 'nuqs'
 import { ETPCoordenation } from './etp-coordenation'
+import { ETPFilterInput } from './etp-filter-input'
 import { GTMembers } from './gt-members'
+import { LoadingSkeleton } from './loading-skeleton'
 
-export async function EPTAccordion() {
-  const etps = await getEtps()
+export function EPTAccordion() {
+  const [filter] = useQueryState('filter')
+
+  const { data: etps = [], isLoading } = useQuery({
+    queryKey: ['etps'],
+    queryFn: getEtps,
+  })
+
+  const filteredETPs = etps.filter((etp) =>
+    filter
+      ? Object.entries(etp).some(
+          ([, value]) =>
+            typeof value === 'string' &&
+            value.toLowerCase().includes(filter.toLowerCase())
+        )
+      : true
+  )
+
+  const hasNoResults = filteredETPs.length < 1
+
+  const numberOfRows = filteredETPs.length
 
   return (
-    <Accordion collapsible type="single">
-      {etps.map(({ name, description, members, text, gtMembers, gtText }) => (
-        <AccordionItem key={name} value={name}>
-          <AccordionTrigger>
-            <div className="flex flex-col gap-1 text-left">
-              <span className="title-3">{name}</span>
-              <span className="font-medium text-base text-muted-foreground">
-                {description}
-              </span>
-            </div>
-          </AccordionTrigger>
-          <AccordionContent className="space-y-7">
-            <p className="text-justify">{text}</p>
+    <div className="space-y-2">
+      <div className="flex w-full flex-col gap-2 sm:flex-row sm:items-center sm:gap-4">
+        <div className="w-full flex-1">
+          <ETPFilterInput />
+        </div>
+        <span className="mt-1 text-right font-medium text-muted-foreground text-xs sm:mt-0 sm:text-left">
+          {numberOfRows} resultado{numberOfRows === 1 ? '' : 's'}
+        </span>
+      </div>
 
-            <ETPCoordenation members={members} />
+      {isLoading && <LoadingSkeleton />}
 
-            <GTMembers gtMembers={gtMembers} />
+      {!isLoading && hasNoResults && (
+        <span className="mt-4 flex py-4 text-muted-foreground text-sm">
+          Nenhum ETP foi cadastrado ainda.
+        </span>
+      )}
 
-            <h3 className="mb-2 font-semibold text-muted-foreground text-sm">
-              Sobre o Grupo de Trabalho
-            </h3>
-            <p className="text-justify">{gtText}</p>
-          </AccordionContent>
-        </AccordionItem>
-      ))}
-    </Accordion>
+      <Accordion collapsible type="single">
+        {filteredETPs.map(
+          ({ name, description, members, text, gtMembers, gtText }) => (
+            <AccordionItem key={name} value={name}>
+              <AccordionTrigger>
+                <div className="flex flex-col gap-1 text-left">
+                  <span className="title-3">{name}</span>
+                  <span className="font-medium text-base text-muted-foreground">
+                    {description}
+                  </span>
+                </div>
+              </AccordionTrigger>
+              <AccordionContent className="space-y-7">
+                <p className="text-justify">{text}</p>
+
+                <ETPCoordenation members={members} />
+
+                <GTMembers gtMembers={gtMembers} />
+
+                <h3 className="mb-2 font-semibold text-muted-foreground text-sm">
+                  Sobre o Grupo de Trabalho
+                </h3>
+                <p className="text-justify">{gtText}</p>
+              </AccordionContent>
+            </AccordionItem>
+          )
+        )}
+      </Accordion>
+    </div>
   )
 }

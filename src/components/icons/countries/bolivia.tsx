c3ded8c0d6e83effdca9d78871c8d16cbb80b7b3
andersonkaiti <anderkaiti@gmail.com>
2025-08-31 20:03:54 -0300

    ✨ feat(icons): Adição de ícones de países.

---
+++ src/components/icons/countries/bolivia.tsx
@@ -0,0 +1,32 @@
+export function BoliviaIcon() {
+  return (
+    <svg
+      aria-label="Bolivia flag"
+      fill="none"
+      height={24}
+      viewBox="0 0 24 24"
+      width={24}
+    >
+      <title>Bolivia</title>
+      <g clipPath="url(#BO_svg__a)">
+        <path
+          d="M24 12a11.98 11.98 0 0 0-.746-4.173L12 7.305.746 7.827A11.973 11.973 0 0 0 0 12c0 1.468.264 2.874.746 4.173L12 16.696l11.254-.521c.482-1.3.746-2.707.746-4.174Z"
+          fill="#FFDA44"
+        />
+        <path
+          d="M12 24c5.16 0 9.558-3.257 11.253-7.826H.746C2.442 20.744 6.84 24 12 24Z"
+          fill="#6DA544"
+        />
+        <path
+          d="M.746 7.826h22.507C21.558 3.256 17.16 0 12 0S2.442 3.257.746 7.826Z"
+          fill="#D80027"
+        />
+      </g>
+      <defs>
+        <clipPath id="BO_svg__a">
+          <path d="M0 0h24v24H0z" fill="#fff" />
+        </clipPath>
+      </defs>
+    </svg>
+  )
+}
---
+++ src/components/icons/countries/portugal.tsx
@@ -0,0 +1,40 @@
+export function PortugalIcon() {
+  return (
+    <svg
+      aria-label="Portugal flag"
+      fill="none"
+      height={24}
+      viewBox="0 0 24 24"
+      width={24}
+    >
+      <title>Portugal</title>
+      <g clipPath="url(#PT_svg__a)">
+        <path
+          d="M0 12c0 5.16 3.256 9.559 7.826 11.254L8.87 12.001 7.826.747C3.256 2.443 0 6.841 0 12.001Z"
+          fill="#6DA544"
+        />
+        <path
+          d="M24 12c0-6.627-5.372-12-12-12-1.468 0-2.874.264-4.174.746v22.508c1.3.482 2.706.746 4.174.746 6.628 0 12-5.373 12-12Z"
+          fill="#D80027"
+        />
+        <path
+          d="M7.826 16.174a4.174 4.174 0 1 0 0-8.347 4.174 4.174 0 0 0 0 8.347Z"
+          fill="#FFDA44"
+        />
+        <path
+          d="M5.478 9.913v2.608a2.348 2.348 0 0 0 4.695 0V9.913H5.478Z"
+          fill="#D80027"
+        />
+        <path
+          d="M7.827 13.304a.784.784 0 0 1-.783-.783v-1.043h1.565v1.043a.784.784 0 0 1-.782.783Z"
+          fill="#F0F0F0"
+        />
+      </g>
+      <defs>
+        <clipPath id="PT_svg__a">
+          <path d="M0 0h24v24H0z" fill="#fff" />
+        </clipPath>
+      </defs>
+    </svg>
+  )
+}

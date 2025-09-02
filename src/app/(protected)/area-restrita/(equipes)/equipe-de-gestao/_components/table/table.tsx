7aa3959c1a3d0d27825a80235f123dba41d7dd6e
andersonkaiti <anderkaiti@gmail.com>
2025-09-02 19:11:06 -0300

    ✨ feat(protected,equipe-de-gestao): Adição de opções de exibição para a tabela de equipes, incluindo filtros para nome e quantidade de membros, além de refatoração do hook de gerenciamento de equipes para suportar a funcionalidade de filtragem.

--- src/app/(protected)/area-restrita/(equipes)/equipe-de-gestao/_components/table/table.tsx
+++ src/app/(protected)/area-restrita/(equipes)/equipe-de-gestao/_components/table/table.tsx
@@ -1,8 +1,9 @@
 'use client'
 
 import { DataTable } from '@components/ui/data-table'
+import type { ColumnDef } from '@tanstack/react-table'
+import { parseAsBoolean, useQueryState } from 'nuqs'
 import type { ITeam } from 'types/team'
-
 import { useManagementTeam } from '../../_hooks/use-management-team.hook'
 import { LoadingSkeleton } from './loading-skeleton'
 import { managementTeamTableColumns } from './management-team-table-columns'
@@ -18,13 +19,35 @@ export default function Table() {
     type: TEAM_TYPE,
   })
 
+  const [hasName] = useQueryState('nome', parseAsBoolean.withDefault(true))
+  const [hasQuantity] = useQueryState(
+    'quantidade',
+    parseAsBoolean.withDefault(true)
+  )
+
   if (isLoading) {
     return <LoadingSkeleton />
   }
 
+  const noneOfThem = !(hasName || hasQuantity)
+
+  const filteredManagementTeamTableColumns: ColumnDef<ITeam>[] = noneOfThem
+    ? []
+    : managementTeamTableColumns.filter((column) => {
+        if (column.id === 'name' && !hasName) {
+          return false
+        }
+
+        if (column.id === 'membersQuantity' && !hasQuantity) {
+          return false
+        }
+
+        return true
+      })
+
   return (
     <DataTable<ITeam, unknown>
-      columns={managementTeamTableColumns}
+      columns={filteredManagementTeamTableColumns}
       data={teams}
       handleRemove={handleRemoveTeam}
     />

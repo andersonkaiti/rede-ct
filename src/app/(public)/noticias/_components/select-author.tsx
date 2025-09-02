12ab5bcf6e719d1493efe8a614c366e15fbaea21
andersonkaiti <anderkaiti@gmail.com>
2025-09-02 19:08:01 -0300

    ✨ feat(public,noticias): Refatoração da página de notícias para incluir opções de filtro, ordenação de notícias e seleção de notícias por autor, além de melhorias no layout.

---
+++ src/app/(public)/noticias/_components/select-author.tsx
@@ -0,0 +1,77 @@
+import { Avatar } from '@components/ui/avatar'
+import { Button } from '@components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@components/ui/dropdown-menu'
+import { Separator } from '@components/ui/separator'
+import { AvatarFallback, AvatarImage } from '@radix-ui/react-avatar'
+import { cn } from '@utils/cn'
+import { UserIcon } from 'lucide-react'
+import { parseAsString, useQueryState } from 'nuqs'
+import type { INews } from 'types/news'
+
+interface ISelectAuthorProps {
+  authors: INews['author'][]
+}
+
+export function SelectAuthor({ authors }: ISelectAuthorProps) {
+  const [authorId, setAuthorId] = useQueryState(
+    'author_id',
+    parseAsString.withDefault('')
+  )
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button className="text-muted-foreground" variant="ghost">
+          <UserIcon />
+          {authors.length}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="space-y-1">
+        {authorId && (
+          <>
+            <DropdownMenuItem
+              className={cn('gap-2', !authorId && 'bg-accent/50 font-semibold')}
+              onSelect={() => setAuthorId('')}
+            >
+              <UserIcon className="size-4" />
+              <span className="text-xs">Todos autores</span>
+            </DropdownMenuItem>
+
+            <Separator />
+          </>
+        )}
+
+        {authors.length === 0 ? (
+          <DropdownMenuItem disabled>
+            <span className="text-muted-foreground text-xs">Nenhum autor</span>
+          </DropdownMenuItem>
+        ) : (
+          authors.map(({ id, profile_image_url, first_name, last_name }) => (
+            <DropdownMenuItem
+              className={cn(
+                'gap-2',
+                authorId === id && 'bg-accent/50 font-semibold'
+              )}
+              key={id}
+              onSelect={() => setAuthorId(id)}
+            >
+              <Avatar className="size-6">
+                <AvatarImage src={profile_image_url} />
+                <AvatarFallback>
+                  {first_name?.[0]}
+                  {last_name?.[0]}
+                </AvatarFallback>
+              </Avatar>
+              <span className="truncate text-xs">{`${first_name} ${last_name || ''}`}</span>
+            </DropdownMenuItem>
+          ))
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
--- src/http/news/get-news.ts
+++ src/http/news/get-news.ts
@@ -1,6 +1,22 @@
 import { api } from '@adapters/index'
 import type { INews } from 'types/news'
 
-export async function getNews() {
-  return await api.get<INews[]>('/news')
+interface IGetNewsProps {
+  filter: string
+  orderBy: string
+  authorId: string
+}
+
+export async function getNews({ filter, orderBy, authorId }: IGetNewsProps) {
+  const searchParams = new URLSearchParams()
+
+  searchParams.set('title', filter)
+  searchParams.set('content', filter)
+  searchParams.set('updated_at', filter)
+
+  searchParams.set('order_by', orderBy)
+
+  searchParams.set('author_id', authorId)
+
+  return await api.get<INews[]>(`/news?${searchParams}`)
 }

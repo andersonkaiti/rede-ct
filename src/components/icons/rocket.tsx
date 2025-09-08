c332b9c814928e4a6c22e1338509b847f6ffe144
andersonkaiti <anderkaiti@gmail.com>
2025-09-08 00:07:51 -0300

    :sparkles: feat(icons): Refatoração dos componentes de ícones para utilizar componentes funcionais com hooks, melhorando o controle de animações e adicionando suporte a ref nos ícones BlocksIcon, ConstructionIcon, DownloadIcon, MailCheckIcon e RocketIcon.

--- src/components/icons/rocket.tsx
+++ src/components/icons/rocket.tsx
@@ -4,7 +4,7 @@ import { cn } from '@utils/cn'
 import type { Variants } from 'motion/react'
 import { motion, useAnimation } from 'motion/react'
 import type { HTMLAttributes } from 'react'
-import { forwardRef, useCallback, useImperativeHandle, useRef } from 'react'
+import { useCallback, useImperativeHandle, useRef } from 'react'
 
 const ROCKET_ANIMATION_DURATION_SECONDS = 6
 const ROCKET_ANIMATION_X_OFFSET_1 = 0
@@ -87,6 +87,7 @@ export interface RocketIconHandle {
 
 interface RocketIconProps extends HTMLAttributes<HTMLDivElement> {
   size?: number
+  ref?: React.Ref<RocketIconHandle>
 }
 
 const variants: Variants = {
@@ -129,77 +130,78 @@ const fireVariants: Variants = {
   },
 }
 
-const RocketIcon = forwardRef<RocketIconHandle, RocketIconProps>(
-  ({ onMouseEnter, onMouseLeave, className, size = 28, ...props }, ref) => {
-    const controls = useAnimation()
-    const isControlledRef = useRef(false)
+export function RocketIcon({
+  onMouseEnter,
+  onMouseLeave,
+  className,
+  size = 28,
+  ref,
+  ...props
+}: RocketIconProps) {
+  const controls = useAnimation()
+  const isControlledRef = useRef(false)
 
-    useImperativeHandle(ref, () => {
-      isControlledRef.current = true
+  useImperativeHandle(ref, () => {
+    isControlledRef.current = true
 
-      return {
-        startAnimation: () => controls.start('animate'),
-        stopAnimation: () => controls.start('normal'),
+    return {
+      startAnimation: () => controls.start('animate'),
+      stopAnimation: () => controls.start('normal'),
+    }
+  })
+
+  const handleMouseEnter = useCallback(
+    (e: React.MouseEvent<HTMLDivElement>) => {
+      if (isControlledRef.current) {
+        onMouseEnter?.(e)
+      } else {
+        controls.start('animate')
       }
-    })
+    },
+    [controls, onMouseEnter]
+  )
 
-    const handleMouseEnter = useCallback(
-      (e: React.MouseEvent<HTMLDivElement>) => {
-        if (isControlledRef.current) {
-          onMouseEnter?.(e)
-        } else {
-          controls.start('animate')
-        }
-      },
-      [controls, onMouseEnter]
-    )
+  const handleMouseLeave = useCallback(
+    (e: React.MouseEvent<HTMLDivElement>) => {
+      if (isControlledRef.current) {
+        onMouseLeave?.(e)
+      } else {
+        controls.start('normal')
+      }
+    },
+    [controls, onMouseLeave]
+  )
 
-    const handleMouseLeave = useCallback(
-      (e: React.MouseEvent<HTMLDivElement>) => {
-        if (isControlledRef.current) {
-          onMouseLeave?.(e)
-        } else {
-          controls.start('normal')
-        }
-      },
-      [controls, onMouseLeave]
-    )
-
-    return (
-      <div
-        className={cn(className)}
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-        {...props}
+  return (
+    <div
+      className={cn(className)}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      {...props}
+    >
+      <motion.svg
+        animate={controls}
+        fill="none"
+        height={size}
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        variants={variants}
+        viewBox="0 0 24 24"
+        width={size}
+        xmlns="http://www.w3.org/2000/svg"
       >
-        <motion.svg
+        <title>Rocket Icon</title>
+        <motion.path
           animate={controls}
-          fill="none"
-          height={size}
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          variants={variants}
-          viewBox="0 0 24 24"
-          width={size}
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <title>Rocket Icon</title>
-          <motion.path
-            animate={controls}
-            d="M4.5 16.5c-1.5 1.26-2 5-2 5s3.74-.5 5-2c.71-.84.7-2.13-.09-2.91a2.18 2.18 0 0 0-2.91-.09z"
-            variants={fireVariants}
-          />
-          <path d="m12 15-3-3a22 22 0 0 1 2-3.95A12.88 12.88 0 0 1 22 2c0 2.72-.78 7.5-6 11a22.35 22.35 0 0 1-4 2z" />
-          <path d="M9 12H4s.55-3.03 2-4c1.62-1.08 5 0 5 0" />
-          <path d="M12 15v5s3.03-.55 4-2c1.08-1.62 0-5 0-5" />
-        </motion.svg>
-      </div>
-    )
-  }
-)
-
-RocketIcon.displayName = 'RocketIcon'
-
-export { RocketIcon }
+          d="M4.5 16.5c-1.5 1.26-2 5-2 5s3.74-.5 5-2c.71-.84.7-2.13-.09-2.91a2.18 2.18 0 0 0-2.91-.09z"
+          variants={fireVariants}
+        />
+        <path d="m12 15-3-3a22 22 0 0 1 2-3.95A12.88 12.88 0 0 1 22 2c0 2.72-.78 7.5-6 11a22.35 22.35 0 0 1-4 2z" />
+        <path d="M9 12H4s.55-3.03 2-4c1.62-1.08 5 0 5 0" />
+        <path d="M12 15v5s3.03-.55 4-2c1.08-1.62 0-5 0-5" />
+      </motion.svg>
+    </div>
+  )
+}

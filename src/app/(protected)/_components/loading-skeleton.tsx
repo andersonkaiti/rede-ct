e4a8654c663fd24fad3ab2b2acb472c6f37a97e9
andersonkaiti <anderkaiti@gmail.com>
2025-09-11 09:58:29 -0300

    :lipstick: style(protected,layout,loading-skeleton): Remoção de elementos desnecessários do componente LoadingSkeleton para simplificação.

--- src/app/(protected)/_components/loading-skeleton.tsx
+++ src/app/(protected)/_components/loading-skeleton.tsx
@@ -4,7 +4,7 @@ import { Skeleton } from '@components/ui/skeleton'
 export function LoadingSkeleton() {
   return (
     <div className="flex h-screen w-full">
-      <div className="hidden flex-col border-r bg-background/40 md:flex md:w-76">
+      <div className="hidden flex-col border-r bg-background/40 md:flex md:w-64">
         <div className="flex items-center gap-3 p-4">
           <Skeleton className="size-8 shrink-0 rounded-full" />
           <div className="flex w-full justify-between gap-2">
@@ -30,28 +30,6 @@ export function LoadingSkeleton() {
         <div className="flex h-13 items-center gap-4 px-6 py-4">
           <Skeleton className="size-4" />
           <Skeleton className="h-4 w-14" />
-        </div>
-      </div>
-
-      <div className="flex w-full flex-col">
-        <div className="flex h-16 items-center justify-between p-4">
-          <Skeleton className="size-7" />
-          <Skeleton className="size-7 rounded-full" />
-        </div>
-
-        <div className="mx-auto flex h-full w-full flex-col gap-2 p-4 py-10 md:gap-8">
-          <div className="space-y-2">
-            <Skeleton className="h-9 w-80" />
-            <Skeleton className="h-5 w-full rounded-full" />
-          </div>
-
-          <div className="flex flex-col gap-2">
-            <Skeleton className="h-23.5 w-full" />
-            <Skeleton className="h-23.5 w-full" />
-            <Skeleton className="h-23.5 w-full" />
-            <Skeleton className="h-23.5 w-full" />
-            <Skeleton className="h-23.5 w-full" />
-          </div>
         </div>
       </div>
     </div>

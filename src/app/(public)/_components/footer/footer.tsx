fcc501abddd3aba48795bc093fa4afb7b4ddefef
andersonkaiti <anderkaiti@gmail.com>
2025-09-27 15:53:21 -0300

    :lipstick: style(footer): Adição de uma nova camada de fundo ao componente Footer, melhorando a estética visual com um gradiente radial.

--- src/app/(public)/_components/footer/footer.tsx
+++ src/app/(public)/_components/footer/footer.tsx
@@ -4,7 +4,7 @@ import { SocialMedia } from './_components/social-media'
 
 export function Footer() {
   return (
-    <footer className="bg-background text-background-foreground">
+    <footer className="relative bg-background text-background-foreground">
       <div className="mx-auto w-full max-w-screen-xl space-y-8 p-4 py-6 sm:space-y-20 lg:py-8">
         <div className="gap-16 space-y-10 md:flex md:justify-between">
           <div className="max-w-xs space-y-8">
@@ -39,6 +39,15 @@ export function Footer() {
           <span className="text-sm sm:text-center">© 2025 REDE CT</span>
         </div>
       </div>
+
+      <div
+        aria-hidden
+        className="-translate-x-1/2 pointer-events-none absolute bottom-0 left-1/2 z-0 hidden h-48 w-full dark:flex"
+        style={{
+          background:
+            'radial-gradient(ellipse 60% 50% at 50% 100%, oklch(0.637 0.237 25.331 / 0.15) 0%, transparent 80%)',
+        }}
+      />
     </footer>
   )
 }

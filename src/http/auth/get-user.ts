b29ae8cc4545fcea606386b21b2de607a41f041b
andersonkaiti <anderkaiti@gmail.com>
2025-10-10 20:52:51 -0300

    :bug: fix(auth): Melhoria no tratamento de erros na função getAuthenticatedUser para deslogar caso o token seja inválido.

--- src/http/auth/get-user.ts
+++ src/http/auth/get-user.ts
@@ -1,4 +1,5 @@
 import { api } from '@http/api-client'
+import { HTTPError } from 'ky'
 import { redirect } from 'next/navigation'
 
 interface IAuthenticatedUserResponse {
@@ -17,7 +18,15 @@ interface IAuthenticatedUserResponse {
 export async function getAuthenticatedUser(): Promise<IAuthenticatedUserResponse> {
   try {
     return await api.get('auth/user').json()
-  } catch {
+  } catch (err) {
+    if (err instanceof HTTPError) {
+      const errorBody = await err.response.json()
+
+      if (errorBody.invalid) {
+        redirect('/api/auth/sign-out')
+      }
+    }
+
     redirect('/')
   }
 }

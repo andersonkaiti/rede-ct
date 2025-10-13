402eae03b5dd3efc7a934503791e78225a62dd76
andersonkaiti <anderkaiti@gmail.com>
2025-10-12 21:48:15 -0300

    :sparkles: feat(opengraph,news): Implementação de geração de imagem OpenGraph para a página de notícia.

---
+++ src/app/(public)/noticias/[id]/opengraph-image.tsx
@@ -0,0 +1,50 @@
+/** biome-ignore-all lint/nursery/useImageSize: needed */
+/** biome-ignore-all lint/performance/noImgElement: needed */
+
+import { getNewsById } from '@http/news/get-news-by-id'
+import { ImageResponse } from 'next/og'
+
+export const alt = 'About Acme'
+
+export const size = {
+  width: 1200,
+  height: 630,
+}
+
+export const contentType = 'image/png'
+
+interface IImageProps {
+  params: Promise<{ id: string }>
+}
+
+export default async function Image({ params }: IImageProps) {
+  const { id } = await params
+
+  const { imageUrl, title } = await getNewsById(id)
+
+  return new ImageResponse(
+    <div
+      style={{
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        overflow: 'hidden',
+        background: '#111',
+      }}
+    >
+      <img
+        alt={title}
+        src={imageUrl}
+        style={{
+          width: '100%',
+          height: '100%',
+          objectFit: 'cover',
+          objectPosition: 'center',
+          display: 'block',
+        }}
+      />
+    </div>
+  )
+}

23499e733ab7c16ada4381d45e8c60e1c6d0b1fa
andersonkaiti <anderkaiti@gmail.com>
2025-10-27 13:07:51 -0300

    :sparkles: feat(etps,accordion): Adição do componente ETPAccordionItem para modularizar a exibição de ETPs, incluindo coordenação e membros, e melhora a estrutura do ETPAccordion.

--- src/app/(public)/quem-somos/eixos-tematicos-permanentes/etps/_components/etp-accordion.tsx
+++ src/app/(public)/quem-somos/eixos-tematicos-permanentes/etps/_components/etp-accordion.tsx
@@ -1,28 +1,22 @@
 'use client'
 
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from '@components/ui/accordion'
-import { getEtps } from '@mocks/etps'
+import { Accordion } from '@components/ui/accordion'
+import { getEtps } from '@http/etps/get-etps'
 import { useQuery } from '@tanstack/react-query'
 import { useQueryState } from 'nuqs'
-import { ETPCoordenation } from './etp-coordenation'
+import { ETPAccordionItem } from './etp-accordion-item'
 import { ETPFilterInput } from './etp-filter-input'
-import { GTMembers } from './gt-members'
 import { LoadingSkeleton } from './loading-skeleton'
 
 export function EPTAccordion() {
   const [filter] = useQueryState('filter')
 
-  const { data: etps = [], isLoading } = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: ['etps'],
-    queryFn: getEtps,
+    queryFn: async () => await getEtps({}),
   })
 
-  const filteredETPs = etps.filter((etp) =>
+  const filteredETPs = data?.etps.filter((etp) =>
     filter
       ? Object.entries(etp).some(
           ([, value]) =>
@@ -32,10 +26,6 @@ export function EPTAccordion() {
       : true
   )
 
-  const hasNoResults = filteredETPs.length < 1
-
-  const numberOfRows = filteredETPs.length
-
   return (
     <div className="space-y-2">
       <div className="flex w-full flex-col gap-2 sm:flex-row sm:items-center sm:gap-4">
@@ -43,54 +33,23 @@ export function EPTAccordion() {
           <ETPFilterInput />
         </div>
         <span className="mt-1 text-right font-medium text-muted-foreground text-xs sm:mt-0 sm:text-left">
-          {numberOfRows} resultado{numberOfRows === 1 ? '' : 's'}
+          {filteredETPs?.length} resultado
+          {filteredETPs?.length === 1 ? '' : 's'}
         </span>
       </div>
 
       {isLoading && <LoadingSkeleton />}
 
-      {!isLoading && hasNoResults && (
+      {!isLoading && filteredETPs?.length === 0 && (
         <span className="mt-4 flex py-4 text-muted-foreground text-sm">
           Nenhum ETP foi cadastrado ainda.
         </span>
       )}
 
       <Accordion collapsible type="single">
-        {filteredETPs.map(
-          ({
-            code,
-            description,
-            gtText,
-            id,
-            members,
-            text,
-            title,
-            gtMembers,
-          }) => (
-            <AccordionItem key={id} value={code}>
-              <AccordionTrigger>
-                <div className="flex flex-col gap-1 text-left">
-                  <span className="title-3">{title}</span>
-                  <span className="font-medium text-base text-muted-foreground">
-                    {description}
-                  </span>
-                </div>
-              </AccordionTrigger>
-              <AccordionContent className="space-y-7">
-                <p className="text-justify">{text}</p>
-
-                <ETPCoordenation members={members} />
-
-                <GTMembers gtMembers={gtMembers} />
-
-                <h3 className="mb-2 font-semibold text-muted-foreground text-sm">
-                  Sobre o Grupo de Trabalho
-                </h3>
-                <p className="text-justify">{gtText}</p>
-              </AccordionContent>
-            </AccordionItem>
-          )
-        )}
+        {filteredETPs?.map((etp) => (
+          <ETPAccordionItem etp={etp} key={etp.id} />
+        ))}
       </Accordion>
     </div>
   )

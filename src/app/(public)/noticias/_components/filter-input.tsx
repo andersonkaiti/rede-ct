343d6d8b28028d823e42504615a96b650542290c
andersonkaiti <anderkaiti@gmail.com>
2025-10-29 19:33:15 -0300

    :sparkles: feat(filter-input): Refatoração dos componentes FilterInput para usar InputGroup e aprimorar o gerenciamento do estado da query.

--- src/app/(public)/noticias/_components/filter-input.tsx
+++ src/app/(public)/noticias/_components/filter-input.tsx
@@ -1,17 +1,24 @@
 'use client'
 
 import { Button } from '@components/ui/button'
-import { Input } from '@components/ui/input'
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from '@components/ui/input-group'
 import { cn } from '@utils/cn'
 import { ArrowDownUp, Search } from 'lucide-react'
-import { parseAsString, useQueryState } from 'nuqs'
+import { parseAsString, useQueryStates } from 'nuqs'
 
 export function FilterInput() {
-  const [filter, setFilter] = useQueryState(
-    'filtro',
-    parseAsString.withDefault('')
-  )
-  const [orderBy = 'desc', setOrderBy] = useQueryState('order_by')
+  const [{ filtro: filter = '', order_by: orderBy = 'desc' }, setQuery] =
+    useQueryStates({
+      filtro: parseAsString.withDefault(''),
+      order_by: parseAsString.withDefault('desc'),
+    })
+
+  const setFilter = (value: string) => setQuery({ filtro: value })
+  const setOrderBy = (value: string) => setQuery({ order_by: value })
 
   function handleOrderBy() {
     setOrderBy(orderBy === 'asc' ? 'desc' : 'asc')
@@ -19,17 +26,17 @@ export function FilterInput() {
 
   return (
     <>
-      <div className="relative w-full sm:w-fit">
-        <Input
-          className="w-full ps-9 sm:w-fit"
+      <InputGroup className="w-full sm:w-fit">
+        <InputGroupAddon>
+          <Search className="size-3" />
+        </InputGroupAddon>
+
+        <InputGroupInput
           onChange={(event) => setFilter(event.currentTarget.value)}
           placeholder="Filtrar"
           value={filter}
         />
-        <div className="pointer-events-none absolute inset-0 start-0 flex w-fit items-center ps-3">
-          <Search className="size-3" />
-        </div>
-      </div>
+      </InputGroup>
 
       <Button
         aria-label={`Ordenar por data ${orderBy === 'asc' ? 'crescente' : 'decrescente'}`}

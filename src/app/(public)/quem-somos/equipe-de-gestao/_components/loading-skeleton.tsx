db884b45ec38305fa963304e093252b91614561f
andersonkaiti <anderkaiti@gmail.com>
2025-11-04 20:55:00 -0300

    :recycle: refactor(public,equipe-de-gestao): Adição do componente de carregamento e melhorias na estrutura e tipagem dos componentes relacionados à gestão de equipes.

--- src/app/(public)/quem-somos/equipe-de-gestao/_components/loading-skeleton.tsx
+++ src/app/(public)/quem-somos/equipe-de-gestao/_components/loading-skeleton.tsx
@@ -1,13 +1,13 @@
 import { Skeleton } from '@components/ui/skeleton'
 import { UserCardSkeleton } from '@components/ui/user-card'
 
-const SECTIONS = 2
-const MEMBERS_PER_SECTION = 6
+const SECTIONS_COUNT = 2
+const MEMBERS_PER_SECTION_COUNT = 6
 
 export default function LoadingSkeleton() {
   return (
     <div className="flex flex-col justify-center gap-12.5">
-      {[...new Array(SECTIONS)].map((_, sectionIdx) => (
+      {Array.from({ length: SECTIONS_COUNT }).map((_, sectionIdx) => (
         <section className="space-y-4 md:space-y-8" key={sectionIdx}>
           <div className="space-y-8">
             <Skeleton className="mx-auto h-10 w-60 rounded-full px-4 text-center" />
@@ -15,9 +15,11 @@ export default function LoadingSkeleton() {
           </div>
 
           <div className="grid w-full grid-cols-1 gap-10 md:grid-cols-3">
-            {[...new Array(MEMBERS_PER_SECTION)].map((__, memberIdx) => (
-              <UserCardSkeleton key={memberIdx} />
-            ))}
+            {Array.from({ length: MEMBERS_PER_SECTION_COUNT }).map(
+              (__, memberIdx) => (
+                <UserCardSkeleton key={memberIdx} />
+              )
+            )}
           </div>
         </section>
       ))}

dcf6517afa3a5551e1519d9b140a290783504f46
andersonkaiti <anderkaiti@gmail.com>
2025-11-05 01:06:02 -0300

    :recycle: refactor(equipes,loading-skeleton): Simplificação dos componentes LoadingSkeleton da seção de equipes, removendo elementos desnecessários.

--- src/app/(protected)/area-restrita/(equipes)/equipe-de-gestao/_components/table/loading-skeleton.tsx
+++ src/app/(protected)/area-restrita/(equipes)/equipe-de-gestao/_components/table/loading-skeleton.tsx
@@ -3,34 +3,23 @@ import { Skeleton } from '@components/ui/skeleton'
 const SKELETON_COUNT = 8
 
 export function LoadingSkeleton() {
-  return (
-    <>
-      <div className="w-full rounded-md border [&_div:last-child]:border-0 [&_div]:border-b">
-        <div className="flex h-[40px] gap-4 px-2 py-3">
-          <Skeleton className="h-full flex-4 rounded-md" />
-          <Skeleton className="h-full flex-3 rounded-md" />
-          <Skeleton className="h-full flex-1 rounded-md" />
-        </div>
-        {Array.from({ length: SKELETON_COUNT }).map((_, index: number) => (
-          <div
-            className="flex h-[52.8px] items-center gap-4 px-2 py-3"
-            key={index}
-          >
-            <Skeleton className="h-2/3 flex-4 rounded-md" />
-            <Skeleton className="h-2/3 flex-3 rounded-md" />
-            <Skeleton className="h-2/3 flex-1 rounded-md" />
-          </div>
-        ))}
-      </div>
-
-      <div className="mt-4 flex items-center justify-between">
-        <Skeleton className="h-9 w-52" />
-        <div className="flex items-center gap-2">
-          <Skeleton className="h-9 w-9" />
-          <Skeleton className="h-9 w-9" />
-          <Skeleton className="h-9 w-9" />
-        </div>
-      </div>
-    </>
-  )
+	return (
+		<div className="w-full rounded-md border [&_div:last-child]:border-0 [&_div]:border-b">
+			<div className="flex h-[40px] gap-4 px-2 py-3">
+				<Skeleton className="h-full flex-4 rounded-md" />
+				<Skeleton className="h-full flex-3 rounded-md" />
+				<Skeleton className="h-full flex-1 rounded-md" />
+			</div>
+			{Array.from({ length: SKELETON_COUNT }).map((_, index: number) => (
+				<div
+					className="flex h-[52.8px] items-center gap-4 px-2 py-3"
+					key={index}
+				>
+					<Skeleton className="h-2/3 flex-4 rounded-md" />
+					<Skeleton className="h-2/3 flex-3 rounded-md" />
+					<Skeleton className="h-2/3 flex-1 rounded-md" />
+				</div>
+			))}
+		</div>
+	)
 }

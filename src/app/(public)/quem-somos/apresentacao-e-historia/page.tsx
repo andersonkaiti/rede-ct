dd54d31bc5e4fd913a43a99f9f6e4d8bc100236d
andersonkaiti <anderkaiti@gmail.com>
2025-11-27 23:14:45 -0300

    :sparkles: feat(spotlight): Adição do componente Spotlight e integração dele nas páginas HeroSection e ApresentacaoEHistoria para aprimorar os efeitos visuais.

--- src/app/(public)/quem-somos/apresentacao-e-historia/page.tsx
+++ src/app/(public)/quem-somos/apresentacao-e-historia/page.tsx
@@ -3,6 +3,7 @@ import { ClockIcon } from '@components/icons/clock'
 import { EarthIcon } from '@components/icons/earth'
 import { Badge } from '@components/ui/badge'
 import { NavigationCard } from '@components/ui/navigation-card'
+import { Spotlight } from '@components/ui/spotlight'
 import {
   Timeline,
   TimelineContent,
@@ -24,135 +25,132 @@ export default async function ApresentacaoEHistoria() {
   const timelineData = await getTimelineRedeCT()
 
   return (
-    <main className="relative mx-auto flex max-w-7xl flex-col justify-center gap-12.5 p-5 py-8 lg:p-25">
-      <div
-        aria-hidden
-        className="-translate-x-1/2 pointer-events-none absolute top-0 left-1/2 z-0 hidden h-48 w-full dark:flex"
-        style={{
-          background:
-            'radial-gradient(ellipse 60% 100% at 50% 0%, oklch(0.637 0.237 25.331 / 0.25) 0%, transparent 80%)',
-        }}
-      />
+    <>
+      <Spotlight intensity="LOW" />
 
-      <section className="space-y-8 px-4 py-16 text-white">
-        <Image
-          alt="Rede CT"
-          className="mx-auto invert-100 dark:invert-0"
-          height={75}
-          priority
-          src="/images/favicon.png"
-          width={75}
-        />
+      <main className="relative mx-auto flex max-w-7xl flex-col justify-center gap-12.5 p-5 py-8 lg:p-25">
+        <section className="space-y-8 px-4 py-16 text-white">
+          <Image
+            alt="Rede CT"
+            className="mx-auto invert-100 dark:invert-0"
+            height={75}
+            priority
+            src="/images/favicon.png"
+            width={75}
+          />
 
-        <h1 className="title-2 text-center text-foreground">RedeCT</h1>
-      </section>
+          <h1 className="title-2 text-center text-foreground">RedeCT</h1>
+        </section>
 
-      <section className="space-y-14">
-        <div className="grid grid-cols-1 gap-10 md:grid-cols-2">
-          <WhatAreRedeCTCard />
+        <section className="space-y-14">
+          <div className="grid grid-cols-1 gap-10 md:grid-cols-2">
+            <WhatAreRedeCTCard />
 
-          <WhatAreTraditionalPeopleCard />
-        </div>
-      </section>
+            <WhatAreTraditionalPeopleCard />
+          </div>
+        </section>
 
-      <section className="space-y-14">
-        <div className="flex items-center justify-center gap-4">
-          <Badge className="rounded-full bg-primary/10 p-1.5 text-primary">
-            <EarthIcon className="!size-7" />
-          </Badge>
-          <h2 className="font-semibold text-3xl">Presença internacional</h2>
-        </div>
+        <section className="space-y-14">
+          <div className="flex items-center justify-center gap-4">
+            <Badge className="rounded-full bg-primary/10 p-1.5 text-primary">
+              <EarthIcon className="!size-7" />
+            </Badge>
+            <h2 className="font-semibold text-3xl">Presença internacional</h2>
+          </div>
 
-        <Countries />
-      </section>
+          <Countries />
+        </section>
 
-      <section className="space-y-14">
-        <div className="flex items-center gap-4">
-          <Badge className="rounded-full bg-primary/10 p-1.5 text-primary">
-            <ClockIcon className="!size-7" />
-          </Badge>
-          <h2 className="font-semibold text-3xl">História</h2>
-        </div>
-        <div className="space-y-7">
-          <p className="text-justify">
-            A história da RedeCT tem raízes nos trabalhos de seus idealizadores,
-            remontando ao ano de 2002, quando professores, estudantes e
-            moradores de comunidades tradicionais amazônicas realizaram uma
-            associação que objetivava a promoção do desenvolvimento sustentável
-            de comunidades, especialmente povos tradicionais.
-          </p>
-          <p className="text-justify">
-            Em 2014, já na Universidade Estadual Paulista - UNESP (Tupã/SP), o
-            professor Nelson Russo de Moraes convidou diversos pesquisadores,
-            professores e estudantes à criação do grupo de pesquisa GEDGS (Grupo
-            de Estudos em Democracia e Gestão Social).
-          </p>
-          <p className="text-justify">
-            Em 22/05/2018, no X ENAPEGS (Encontro Nacional de Pesquisadores em
-            Gestão Social), realizado na Universidade Federal do Cariri – UFCA
-            (Juazeiro do Norte – CE/Brasil) foi oficialmente apresentada, de
-            modo público, a Rede Internacional de Pesquisadores sobre Povos
-            Originários e Comunidades Tradicionais – RedeCT.
-          </p>
-        </div>
-      </section>
+        <section className="space-y-14">
+          <div className="flex items-center gap-4">
+            <Badge className="rounded-full bg-primary/10 p-1.5 text-primary">
+              <ClockIcon className="!size-7" />
+            </Badge>
+            <h2 className="font-semibold text-3xl">História</h2>
+          </div>
+          <div className="space-y-7">
+            <p className="text-justify">
+              A história da RedeCT tem raízes nos trabalhos de seus
+              idealizadores, remontando ao ano de 2002, quando professores,
+              estudantes e moradores de comunidades tradicionais amazônicas
+              realizaram uma associação que objetivava a promoção do
+              desenvolvimento sustentável de comunidades, especialmente povos
+              tradicionais.
+            </p>
+            <p className="text-justify">
+              Em 2014, já na Universidade Estadual Paulista - UNESP (Tupã/SP), o
+              professor Nelson Russo de Moraes convidou diversos pesquisadores,
+              professores e estudantes à criação do grupo de pesquisa GEDGS
+              (Grupo de Estudos em Democracia e Gestão Social).
+            </p>
+            <p className="text-justify">
+              Em 22/05/2018, no X ENAPEGS (Encontro Nacional de Pesquisadores em
+              Gestão Social), realizado na Universidade Federal do Cariri – UFCA
+              (Juazeiro do Norte – CE/Brasil) foi oficialmente apresentada, de
+              modo público, a Rede Internacional de Pesquisadores sobre Povos
+              Originários e Comunidades Tradicionais – RedeCT.
+            </p>
+          </div>
+        </section>
 
-      <section className="space-y-14">
-        <div className="flex items-center gap-4">
-          <Badge className="rounded-full bg-primary/10 p-1.5 text-primary">
-            <BookTextIcon className="!size-7" />
-          </Badge>
-          <h2 className="font-semibold text-3xl">Publicações</h2>
-        </div>
-        <div className="space-y-7">
-          <p className="text-justify">
-            Um dos primeiros passos da RedeCT foi o lançamento, em julho de 2018
-            de um edital de chamamento de capítulos de livros para a composição
-            de uma primeira coletânea de trabalhos científicos acerca dos Povos
-            Tradicionais (povos originários e comunidades tradicionais) e suas
-            demandas. A publicação seguiu fluxo anual e em 2023 a RedeCT
-            publicou o 12º volume, chegando a mais de 150 trabalhos publicados.
-          </p>
-        </div>
-      </section>
+        <section className="space-y-14">
+          <div className="flex items-center gap-4">
+            <Badge className="rounded-full bg-primary/10 p-1.5 text-primary">
+              <BookTextIcon className="!size-7" />
+            </Badge>
+            <h2 className="font-semibold text-3xl">Publicações</h2>
+          </div>
+          <div className="space-y-7">
+            <p className="text-justify">
+              Um dos primeiros passos da RedeCT foi o lançamento, em julho de
+              2018 de um edital de chamamento de capítulos de livros para a
+              composição de uma primeira coletânea de trabalhos científicos
+              acerca dos Povos Tradicionais (povos originários e comunidades
+              tradicionais) e suas demandas. A publicação seguiu fluxo anual e
+              em 2023 a RedeCT publicou o 12º volume, chegando a mais de 150
+              trabalhos publicados.
+            </p>
+          </div>
+        </section>
 
-      <section className="space-y-14">
-        <div className="flex items-center justify-center gap-4">
-          <Badge className="rounded-full bg-primary/10 p-1.5 text-primary">
-            <ClockIcon />
-          </Badge>
-          <h2 className="title-3">Linha do tempo da RedeCT</h2>
-        </div>
-        <div className="relative w-full space-y-7 sm:space-y-14">
-          <Timeline defaultValue={timelineData.length}>
-            {timelineData.map((item, index: number) => (
-              <TimelineItem key={index} step={index + 1}>
-                <TimelineIndicator />
-                <TimelineHeader>
-                  <TimelineDate>{item.date}</TimelineDate>
-                  <TimelineTitle>{item.title}</TimelineTitle>
-                </TimelineHeader>
-                <TimelineContent>
-                  <p className="text-justify text-muted-foreground">
-                    {item.text}
-                  </p>
-                </TimelineContent>
-                <TimelineSeparator />
-              </TimelineItem>
-            ))}
-          </Timeline>
-        </div>
-      </section>
+        <section className="space-y-14">
+          <div className="flex items-center justify-center gap-4">
+            <Badge className="rounded-full bg-primary/10 p-1.5 text-primary">
+              <ClockIcon />
+            </Badge>
+            <h2 className="title-3">Linha do tempo da RedeCT</h2>
+          </div>
+          <div className="relative w-full space-y-7 sm:space-y-14">
+            <Timeline defaultValue={timelineData.length}>
+              {timelineData.map((item, index: number) => (
+                <TimelineItem key={index} step={index + 1}>
+                  <TimelineIndicator />
+                  <TimelineHeader>
+                    <TimelineDate>{item.date}</TimelineDate>
+                    <TimelineTitle>{item.title}</TimelineTitle>
+                  </TimelineHeader>
+                  <TimelineContent>
+                    <p className="text-justify text-muted-foreground">
+                      {item.text}
+                    </p>
+                  </TimelineContent>
+                  <TimelineSeparator />
+                </TimelineItem>
+              ))}
+            </Timeline>
+          </div>
+        </section>
 
-      <NavigationCard
-        href="/quem-somos/apresentacao-e-historia/filiacao"
-        variant="red"
-      >
-        <h2 className="title-3 flex items-center gap-2">
-          <Users />
-          Como se filiar à RedeCT?
-        </h2>
-      </NavigationCard>
-    </main>
+        <NavigationCard
+          href="/quem-somos/apresentacao-e-historia/filiacao"
+          variant="red"
+        >
+          <h2 className="title-3 flex items-center gap-2">
+            <Users />
+            Como se filiar à RedeCT?
+          </h2>
+        </NavigationCard>
+      </main>
+    </>
   )
 }

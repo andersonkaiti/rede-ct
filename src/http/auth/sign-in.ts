6bebdd29bd2645f461c9f63161144b48ff27c992
andersonkaiti <anderkaiti@gmail.com>
2025-12-07 12:06:17 -0300

    :fire: refactor(sign-in): Remoção da action de sign-in e atualização do hook e componente de formulário para gerenciar diretamente o envio e a resposta, incluindo melhorias na manipulação de erros e no uso de cookies.

--- src/http/auth/sign-in.ts
+++ src/http/auth/sign-in.ts
@@ -1,18 +1,21 @@
 import { api } from '@http/api-client'
+import z from 'zod'
 
 interface ISignInRequest {
   email: string
   password: string
 }
 
-interface ISignResponse {
-  token: string
-}
+const signInSchema = z.object({
+  token: z.string(),
+})
 
-export async function signIn(data: ISignInRequest): Promise<ISignResponse> {
-  return await api
+export async function signIn(data: ISignInRequest) {
+  const result = await api
     .post('auth/sign-in', {
       json: data,
     })
     .json()
+
+  return signInSchema.parse(result)
 }

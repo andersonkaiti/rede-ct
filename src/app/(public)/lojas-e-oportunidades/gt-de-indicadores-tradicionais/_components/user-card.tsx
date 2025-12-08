403a5d1d2fddedc0be53f4a1018ebdc6e556f3e9
andersonkaiti <anderkaiti@gmail.com>
2025-12-07 21:58:45 -0300

    :sparkles: feat(public,gt-de-indicadores-tradicionais): Implementação da página de Indicadores Tradicionais com novos componentes, incluindo LoadingSkeleton e UserCard, além de integração dinâmica com o grupo de trabalho.

---
+++ src/app/(public)/lojas-e-oportunidades/gt-de-indicadores-tradicionais/_components/user-card.tsx
@@ -0,0 +1,44 @@
+import { UserCard, UserCardImage } from '@components/ui/user-card'
+import UserProfileHoverCard from '@components/user-profile-hover-card'
+
+interface UserCardComponentProps {
+  member: {
+    id: string
+    role: string
+    description: string | null
+    createdAt: string
+    updatedAt: string
+    userId: string
+    user: {
+      id: string
+      name: string
+      avatarUrl: string | null
+      createdAt: string
+      updatedAt: string
+      emailAddress: string
+      orcid: string | null
+      phone: string | null
+      lattesUrl: string | null
+      role: 'ADMIN' | 'USER'
+    }
+  }
+}
+
+export function UserCardComponent({ member }: UserCardComponentProps) {
+  return (
+    <UserCard>
+      <UserCardImage
+        alt={member.user.name}
+        src={member.user.avatarUrl as string}
+      />
+      <div className="flex grow flex-col items-center justify-between gap-1">
+        <h1 className="text-center font-bold text-xl">
+          <UserProfileHoverCard avatarVisibility={false} user={member.user} />
+        </h1>
+        <h2 className="text-center font-semibold text-muted-foreground text-xs">
+          {member.role}
+        </h2>
+      </div>
+    </UserCard>
+  )
+}

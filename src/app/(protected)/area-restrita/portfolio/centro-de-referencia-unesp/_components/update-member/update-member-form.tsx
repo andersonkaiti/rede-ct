a223f9396277c40552e56b19c8110680f6ef1da5
andersonkaiti <anderkaiti@gmail.com>
2025-12-09 00:11:42 -0300

    :sparkles: feat(protected,centro-de-referencia): Implementação do formulário de atualização de membro da equipe, incluindo validação, integração com a API e tratamento de erros.

---
+++ src/app/(protected)/area-restrita/portfolio/centro-de-referencia-unesp/_components/update-member/update-member-form.tsx
@@ -0,0 +1,119 @@
+import { Alert, AlertDescription } from '@components/ui/alert'
+import { Button } from '@components/ui/button'
+import {
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '@components/ui/dialog'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@components/ui/form'
+import { Input } from '@components/ui/input'
+import { Label } from '@components/ui/label'
+import { PageFormContentField } from '@components/ui/page-container'
+import { Textarea } from '@components/ui/textarea'
+import { AlertCircle, Loader2 } from 'lucide-react'
+import { SelectMember } from '../../../../_components/select-member'
+import { useUpdateReferenceCenterTeamMember } from './use-update-member.hook'
+
+interface IUpdateMemberFormProps {
+  setIsOpen: (isOpen: boolean) => void
+}
+
+export function UpdateMemberForm({ setIsOpen }: IUpdateMemberFormProps) {
+  const { form, isSubmitting, member, submit, serverError } =
+    useUpdateReferenceCenterTeamMember({
+      setIsOpen,
+    })
+
+  if (!member) {
+    return null
+  }
+
+  return (
+    <DialogContent className="max-h-[100vh-2rem] space-y-8 overflow-y-auto">
+      <DialogHeader>
+        <DialogTitle>Atualizar membro da equipe</DialogTitle>
+      </DialogHeader>
+
+      <DialogContent>
+        <Form {...form}>
+          <form className="space-y-6" onSubmit={submit}>
+            {serverError && (
+              <Alert className="mb-4 border-primary" variant="destructive">
+                <AlertCircle className="size-4" />
+                <AlertDescription>{serverError}</AlertDescription>
+              </Alert>
+            )}
+
+            <FormField
+              control={form.control}
+              name="userId"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Membro</FormLabel>
+                  <FormControl>
+                    <SelectMember
+                      onChange={field.onChange}
+                      userId={member.userId}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="role"
+              render={({ field }) => (
+                <PageFormContentField>
+                  <Label>Cargo</Label>
+                  <Input
+                    {...field}
+                    onChange={field.onChange}
+                    placeholder="Cargo"
+                  />
+                  <FormMessage />
+                </PageFormContentField>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <PageFormContentField>
+                  <Label>Descrição</Label>
+                  <Textarea
+                    {...field}
+                    onChange={field.onChange}
+                    placeholder="Descrição"
+                  />
+                  <FormMessage />
+                </PageFormContentField>
+              )}
+            />
+
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant="ghost">Cancelar</Button>
+              </DialogClose>
+              <Button disabled={isSubmitting} type="submit" variant="outline">
+                {isSubmitting && <Loader2 className="size-4 animate-spin" />}
+                Atualizar membro
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </DialogContent>
+  )
+}
---
+++ src/http/teams/reference-center-team/get-reference-center-member-by-id.ts
@@ -0,0 +1,29 @@
+import { api } from '@http/api-client'
+import z from 'zod'
+
+const referenceCenterTeamMemberSchema = z.object({
+  id: z.string(),
+  role: z.string(),
+  description: z.string().nullable(),
+  createdAt: z.string(),
+  updatedAt: z.string(),
+  userId: z.string(),
+  user: z.object({
+    id: z.string(),
+    name: z.string(),
+    avatarUrl: z.string().nullable(),
+    createdAt: z.string(),
+    updatedAt: z.string(),
+    emailAddress: z.string(),
+    orcid: z.string().nullable(),
+    phone: z.string().nullable(),
+    lattesUrl: z.string().nullable(),
+    role: z.enum(['ADMIN', 'USER']),
+  }),
+})
+
+export async function getReferenceCenterTeamMemberById(id: string) {
+  const data = await api.get(`reference-center-team-member/${id}`).json()
+
+  return referenceCenterTeamMemberSchema.parse(data)
+}
